b16c78c7f0d81178405e31292e57bb84b840992a
QuwsarOhi <quwsarohi@gmail.com>
2017-07-21 18:11:16 +0600

    lightoj_math

--- temp/con2.cpp
+++ temp/con2.cpp
@@ -1,1 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+// Alice : the player who takes the last loses
+// Bob : the player who takes tha last wins
+
+bool whoWin(int x, bool person, bool flip) {		// 1 = Alice, 0 = Bob, flip == 1 : alice starts the game
+	if(x == 0) {
+		if(flip)
+			return person;
+		return !person;
+	}
+	
+	//if(whoWin(x-1, !person, flip) == person || whoWin(x-2, !person, flip) == person)
+		//return person;
+	
+
+	if(x >= 2 && whoWin(x-2, !person, flip) == person)
+		return person;
+	if(x >= 1 && whoWin(x-1, !person, flip) == person) {
+		//printf("reached here\n");
+		return person;
+	}
+	
+	//printf("Reached last %d %d %d\n", x, (int)person, (int)flip);
+	return !person;
+}
+		
+
+
+int main() {
+	int x, y;
+	bool q;
+	
+	while(1) {
+		scanf("%d %d", &x, &y);
+		if(y == 1) {
+			if(whoWin(x, 1, 1))
+				printf("Alice\n");
+			else
+				printf("Bob\n");
+		}
+		else {
+			if(whoWin(x, 0, 0) == false)
+				printf("Bob\n");
+			else
+				printf("Alice\n");
+		}
+	}
+	return 0;
+}
--- temp/test.cpp
+++ temp/test.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+	int t, x;
+	char s[20];
+	scanf("%d", &t);
+	
+	for(int Case = 1; Case <= t; ++Case) {
+		scanf("%d %s", &x, s);
+		printf("Case %d: ", Case);
+		if(strcmp(s, "Bob") == 0) {
+			if(x%3 == 0)
+				printf("Alice\n");
+			else
+				printf("Bob\n");
+		}
+		else {
+			//printf("Here\n");
+			if((x-1)%3 == 0)
+				printf("Bob\n");
+			else
+				printf("Alice\n");
+		}
+	}
+	return 0;
+}
