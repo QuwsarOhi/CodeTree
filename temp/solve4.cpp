e7cae1064d7ed9c211440175a45366503e23066b
QuwsarOhi <quwsarohi@gmail.com>
2017-08-21 01:44:23 +0600

    pdf

--- temp/solve4.cpp
+++ temp/solve4.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+int main() {
+	long long t, n, a, b;
+	scanf("%lld", &t);
+	
+	while(t--) {
+		scanf("%lld%lld%lld", &n, &a, &b);
+		long long m = (n/2) + 1;
+		if(n & 1)
+			m += 1;
+		long long opp_mid = (a+(m-1))%n;
+		//cout << m << " " << opp_mid << endl;
+		if(b < opp_mid && a != opp_mid && opp_mid > 0 && n != 3) {
+			if(a > b)
+				b += n;
+			//cout << a << " " << b << endl;
+			printf("%lld\n", abs(a-b)-1);
+		}
+		else
+			printf("0\n");
+			
+	}
+	return 0;
+}
