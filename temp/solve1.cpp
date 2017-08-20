580da271afe64544e02f568b16feb0ef9bdc0e8b
QuwsarOhi <quwsarohi@gmail.com>
2017-08-20 21:42:40 +0600

    books

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,94 +1,42 @@
-// 1268 - Unlucky Strings
-// Matrix Exponentiation, Automaton, KMP
-
-// The main problem here is to generate a faulure function
-// To do this, Automaton is used (DFA)
-
-// Build the DFA of the pattern
-// As this pattern cant remain in the string, we'll ignore the finishing state, and all others states will be our finishing state
+// Codeforces
+// C. Leha and Function (429C)
+// Math, Greedy
+// http://codeforces.com/contest/841/problem/C
 
 #include <bits/stdc++.h>
-#define MAXN 52
 using namespace std;
 
-struct matrix {
-	unsigned mat[MAXN][MAXN];
-	matrix() {
-		memset(mat, 0, sizeof mat);
-	}
-};
-
-int t, n, m;
-
-void matprint(matrix a, int x, int y) {
-	for(int i = 0; i < x; ++i) {
-		for(int j = 0; j < y; ++j)
-			printf("%u ", a.mat[i][j]);
-		printf("\n");
-	}
-	printf("\n");
-}
-
-
-matrix matMul(matrix a, matrix b) {
-	matrix tmp;
-	for(int i = 0; i < MAXN; ++i)
-		for(int j = 0; j < MAXN; ++j)
-			for(int k = 0; k < MAXN; ++k)
-				tmp.mat[i][j] += (a.mat[i][k] * b.mat[k][j]);
-	return tmp;
-}
-
-
-matrix matPow(matrix a, int p) {
-	if(p == 1)
-		return a;
-	if(p & 1)
-		return matMul(a, matPow(a, p-1));
-	matrix tmp = matPow(a, p/2);
-	return matMul(tmp, tmp);
-}
-
-matrix failFunc(string P, string Alpha) {
-	matrix f;
-	int P_len = P.size();
-	int A_len = Alpha.size();
-	
-	for(int len = 0; len < P_len; ++len) {
-		for(int j = 0; j < A_len; ++j) {
-			string patt = P.substr(0, len) + Alpha[j];		// Take 0 to n charechters from the pattern, and take one word at a time from alphabet
-			for(int nlen = len + 1; nlen >= 0; --nlen) {	// Try to find the pattern, if not found, reduce the pattern into smaller segment
-				if(nlen == 0 || patt.substr(len+1-nlen, nlen) == P.substr(0, nlen)) {	// If there is no match, then the segment should link to the starting state (nlen == 0)
-					if(nlen != P_len)
-						f.mat[len][nlen]++;
-					break;
-				}
-			}
-		}
-	}
-	return f;
-}
 
 int main() {
+	int m, x;
+	vector<int>a,ai;
+	vector<pair<int, int> >b;
 	
-	//freopen("in", "r", stdin);
-	//freopen("out", "w", stdout);
+	scanf("%d", &m);
 	
-	ios_base::sync_with_stdio(false);
-	
-	cin >> t;
-	string P, Alpha;
-	
-	for(int Case = 1; Case <= t; ++Case) {
-		cin >> n;
-		cin >> Alpha >> P;
-		m = P.size();
-		matrix ff = failFunc(P, Alpha);
-		ff = matPow(ff, n);
-		unsigned ans = 0;
-		for(int i = 0; i < m; ++i)
-			ans += ff.mat[0][i];
-		cout << "Case "  << Case << ": " << ans << endl;
+	for(int i = 0; i < m; ++i) {
+		scanf("%d", &x);
+		a.push_back(x);
 	}
+	
+	for(int i = 0; i < m; ++i) {
+		scanf("%d", &x);
+		b.push_back(make_pair(x, i));
+	}
+	
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	
+	ai.resize(m+10);
+	
+	for(int i = 0, j = m-1; i < m; ++i, --j) {
+		ai[b[i].second] = a[j];
+	}
+	
+	for(int i = 0; i < m-1; ++i) {
+		printf("%d ", ai[i]);
+	}
+	printf("%d\n", ai[m-1]);
+	
 	return 0;
 }
