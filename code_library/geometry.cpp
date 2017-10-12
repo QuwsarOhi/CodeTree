6552565fc13cde5a2b5a2d94bd0141300822c929
QuwsarOhi <quwsarohi@gmail.com>
2017-10-13 01:41:47 +0600

    geom

--- code_library/geometry.cpp
+++ code_library/geometry.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ---------------------------Basic Functions---------------------------
 
 inline bool isEqual(double a, double b) {
 	if(abs(a - b) <= EPS)
@@ -8,16 +9,72 @@ inline bool isEqual(double a, double b) {
 	return 0;
 }
 
+
 inline bool isGreater(double a, double b) {
 	if(a >= b+EPS)
 		return 1;
 	return 0;
 }
 
+
+int Distance(int x1, int y1, int x2, int y2) {			// Without Sqrt
+	int x = x1-x2;
+	int y = y1-y2;
+	return x*x + y*y;
+}
+
+
+double getAngle(double AB, double BC, double CA) {		// Returns Angle(Rad)
+	return acos((AB*AB + BC*BC - CA*CA)/(2*AB*BC));
+}
+
+
+// -------------------------------Triangle-----------------------------
+
 double TriangleArea(double AB, double BC, double CA) {
 	double s = (AB + BC + CA)/2.0;
 	return sqrt(s*(s-AB)*(s-BC)*(s-CA));
 }
+
+
+
+// -------------------------------Circle--------------------------------
+
+struct circle {
+	int x, y, r;
+	circle(int _x, int _y, int _r) {
+		x = _x;
+		y = _y;
+		r = _r;
+	}
+	double Area() {
+		return PI*r*r;
+	}
+};
+
+// Reference: https://www.mathsisfun.com/geometry/circle-sector-segment.html
+double CircleSegmentArea(double r, double angle) {		// Circle Radius, Center Angle(Rad)
+	return r * r * 0.5 * (angle - sin(angle));
+}
+
+
+double CircleSectorArea(double r, double angle) {		// Circle Radius, Center Angle(Rad)
+	return r * r * 0.5 * angle;
+}
+
+
+double CircleArcLength(double r, double angle) {		// Circle Radius, Center Angle(Rad)
+	return r * angle;
+}
+
+
+bool doIntersectCircle(circle c1, circle c2) {
+	int dis = Distance(c1.x, c1.y, c2.x, c2.y);
+	if(sqrt(dis) < c1.r+c2.r)
+		return 1;
+	return 0;
+}
+
 
 
 int main() {
--- temp/solve2.cpp
+++ temp/solve2.cpp
@@ -1,1 +1,65 @@
+// LightOJ
+// 1118 - Incredible Molecules
 
+#include <bits/stdc++.h>
+#define PI acos(-1)
+using namespace std;
+
+struct circle {
+	int x, y, r;
+	circle(int _x, int _y, int _r) {
+		x = _x;
+		y = _y;
+		r = _r;
+	}
+	double Area() {
+		return PI*r*r;
+	}
+};
+
+int Distance(int x1, int y1, int x2, int y2) {			// Without Sqrt
+	int x = x1-x2;
+	int y = y1-y2;
+	return x*x + y*y;
+}
+
+// Reference: https://www.mathsisfun.com/geometry/circle-sector-segment.html
+double CircleSegmentArea(double r, double angle) {		// Circle Radius, Center Angle(Rad)
+	return r * r * 0.5 * (angle - sin(angle));
+}
+
+double getAngle(double AB, double BC, double CA) {		// Returns Angle(Rad)
+	return acos((AB*AB + BC*BC - CA*CA)/(2*AB*BC));
+}
+
+bool doIntersectCircle(circle c1, circle c2) {
+	int dis = Distance(c1.x, c1.y, c2.x, c2.y);
+	if(sqrt(dis) < c1.r+c2.r)
+		return 1;
+	return 0;
+}
+
+int main() {
+	int t, x1, y1, x2, y2, r1, r2;
+	scanf("%d", &t);
+	for(int Case = 1; Case <= t; ++Case) {
+		scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+		printf("Case %d: ", Case);
+		circle c1(x1, y1, r1), c2(x2, y2, r2);
+		if(doIntersectCircle(c1, c2)) {
+			double d = sqrt(Distance(c1.x, c1.y, c2.x, c2.y));
+			if(d + c1.r <= c2.r)
+				printf("%.10lf\n", c1.Area());
+			else if(d + c2.r <= c1.r)
+				printf("%.10lf\n", c2.Area());
+			else {
+				double intersection = CircleSegmentArea(c1.r, 2*getAngle(d, c1.r, c2.r));
+				intersection += CircleSegmentArea(c2.r, 2*getAngle(d, c2.r, c1.r));
+				printf("%.10lf\n", intersection);
+			}
+		}
+		else
+			printf("0\n");
+	}
+	return 0;
+}
