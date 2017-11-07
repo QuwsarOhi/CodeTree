8fc11048f8393da15d0eb45cc013c71f0ac9c3d4
Quwsar Ohi <quwsarohi@gmail.com>
2017-11-07 21:27:52 +0000

    Update solve1.cpp

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,49 +1,174 @@
-// LightOJ
-// 1032 - Fast Bit Calculations
-
 #include <bits/stdc++.h>
-#define isOn(x, i) (x & (1LL<<i))
-#define On(x, i) (x | (1LL<<i))
-
 using namespace std;
+typedef long long ll;
 
-int N, lastBit;
-long long dp[33][33][2][2];
-bitset<5>B;
+vector<pair<ll, ll>>tree;
+string s;
 
-// Bit DP (Almost same as Digit DP)
-// Complexity O(2*pos*total_bits*tights*number_of_bits)
-
-long long bitDP(int pos, int mask, int pairs, bool prevOn, bool tight) {
-    if(pos < 0)
-        return pairs;
-
-    if(dp[pos][pairs][prevOn][tight] != -1)
-        return dp[pos][pairs][prevOn][tight];
+void init(int L, int R, int pos) {
+    if(L == R) {
+        tree[pos].first = s[L]-'0';
+        tree[pos].second = -1;
+        return;
+    }
     
-    long long ans = bitDP(pos-1, mask & ~(1<<pos), pairs, 0, tight && !isOn(mask, pos));
-
-    if(On(mask, pos) <= N)
-        ans += bitDP(pos-1, mask | (1<<pos), pairs + prevOn, 1, tight && isOn(mask, pos));
-        
-    return dp[pos][pairs][prevOn][tight] = ans;
+    int mid = (L+R)>>1;
+    
+    init(L, mid, pos<<1);
+    init(mid+1, R, pos<<1|1);
+    
+    tree[pos].first = tree[pos<<1].first + tree[pos<<1|1].first;
+    tree[pos].second = -1;
 }
 
+void updateOn(int L, int R, int l, int r, int pos) {
+    if(R < l || r < L)
+        return;
+    
+    cout << L << " " << R << endl;
+    
+    if(l <= L && R <= r) {
+        tree[pos].first = (R-L+1);
+        tree[pos].second = 1;
+        return;
+    }
+    
+    int mid = (L+R)>>1;
+    
+    updateOn(L, mid, l, r, pos<<1);
+    updateOn(mid+1, R, l, r, pos<<1|1);
+    
+    tree[pos].first = tree[pos<<1].second == 1 ? (mid-L+1) : (tree[pos<<1].second == 0 ? 0 : tree[pos<<1].first);
+    tree[pos].first += tree[pos<<1|1].second == 1 ? (R-mid) : (tree[pos<<1|1].second == 0 ? 0 : tree[pos<<1|1].first);
+    tree[pos].second = -1;
+}
+
+void updateOff(int L, int R, int l, int r, int pos) {
+    if(R < l || r < L)
+        return;
+    
+    if(l <= L && R <= r) {
+        tree[pos].first = 0;
+        tree[pos].second = 0;
+        return;
+    }
+    
+    int mid = (L+R)>>1;
+    
+    updateOff(L, mid, l, r, pos<<1);
+    updateOff(mid+1, R, l, r, pos<<1|1);
+    
+    tree[pos].first = tree[pos<<1].second == 0 ? 0 : (tree[pos<<1].second == 1 ? (mid-L+1) : tree[pos<<1].first);
+    tree[pos].first += tree[pos<<1|1].second == 0 ? 0 : (tree[pos<<1|1].second == 1 ? (R-mid) : tree[pos<<1|1].first);
+    tree[pos].second = -1;
+}
+
+void updateFlip(int L, int R, int l, int r, int pos, int prop) {
+    if(R < l || r < L)
+        return;
+    
+    cout << L << " " << R << " " << prop << endl;
+    
+    if(L == R) {
+        if(prop == 1)
+            tree[pos].first = 0;
+        else if(prop == 0)
+            tree[pos].first = 1;
+        else
+            tree[pos].first = tree[pos].first == 1 ? 0:1;
+        tree[pos].second = -1;
+        return;
+    }
+    
+    int mid = (L+R)>>1;
+    
+    updateFlip(L, mid, l, r, pos<<1, tree[pos].second);
+    updateFlip(mid+1, R, l, r, pos<<1|1, tree[pos].second);
+    
+    //tree[pos].first = tree[pos].second == -1 ? tree[pos<<1].first : (tree[pos].second == 0 ? 0 : 1);
+    //tree[pos].first += tree[pos].second == -1 ? tree[pos<<1|1].first : (tree[pos].second == 0 ? 0 : 1);
+    //tree[pos].second = -1;
+    tree[pos].first = tree[pos<<1].first + tree[pos<<1|1].first;
+    tree[pos].second = -1;
+}
+
+int queryOn(int L, int R, int l, int r, int pos) {
+    if(R < l || r < L || L > R)
+        return 0;
+    
+    if(L==R)
+        return tree[pos].first;
+    
+    if(l <= L && R <= r) {
+        //cout << "on " << L << " " << R << " - " << tree[pos].second << endl; 
+        if(tree[pos].second != -1)
+            return (R-L+1)*tree[pos].second;
+        else
+            return queryOn(L, (L+R)/2, l, r, pos<<1) + queryOn(((L+R)/2)+1, R, l, r, pos<<1|1);
+    }
+    
+    int mid = (L+R)>>1;
+    
+    int lft = queryOn(L, mid, l, r, pos<<1);
+    int rht = queryOn(mid+1, R, l, r, pos<<1|1);
+    
+    return lft+rht;
+}
+
+void printer(int node) {
+    cout << "NODES" << endl;
+    for(int i = 1; i <= node; ++i) {
+        cout << i << " :: " << tree[i].first << " - " << tree[i].second << endl;
+    }
+}
 
 int main() {
-    int t;
+    //freopen("in", "r", stdin);
+    
+    int t, l, r, x, y, q;
+    char s1[60], c;
+    
     scanf("%d", &t);
     
     for(int Case = 1; Case <= t; ++Case) {
-        scanf("%d", &N);
-        
-        for(int i = 0; i <= 31; ++i)
-            if(isOn(N, i))
-                lastBit = i;
-        
-        memset(dp, -1, sizeof dp);
-        cout << lastBit << endl;
-        printf("Case %d: %lld\n", Case, bitDP(lastBit, N, 0, 0, 1));
+        scanf("%d", &x);
+        s = " ";
+        while(x--) {
+            scanf("%d", &y);
+            scanf(" %s", s1);
+            while(y--)
+                s += string(s1);
+        }
+        tree.resize(s.size()*4);
+        init(1, (int)s.size(), 1);
+        printf("Case %d:\n", Case);
+        scanf("%d", &q);
+        int Query = 1;
+        cout << "1 to " << s.size() << endl;
+        while(q--) {
+            scanf(" %c %d %d", &c, &l, &r);
+            ++l, ++r;
+            if(c == 'F')
+                updateOn(1, (int)s.size()-1, l, r, 1);
+            else if(c == 'E')
+                updateOff(1, (int)s.size()-1, l, r, 1);
+            else if(c == 'I')
+                updateFlip(1, (int)s.size()-1, l, r, 1, tree[1].second);
+            else {
+                printf("Q%d: ", Query++);
+                printf("%d\n", queryOn(1, (int)s.size()-1, l, r, 1));
+            }
+            printer(12);
+        }
     }
+    
     return 0;
 }
+
+/*
+1
+1
+3
+10
+100
+*/
