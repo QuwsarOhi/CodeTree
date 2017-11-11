dd8d3bac28392d5138d1c13891bd815e522168fc
Quwsar Ohi <quwsarohi@gmail.com>
2017-11-11 10:52:23 +0000

    Update SegmentTree.cpp

--- code_library/SegmentTree.cpp
+++ code_library/SegmentTree.cpp
@@ -71,7 +71,7 @@ int segment_query(int pos, int L, int R, int l, int r) {
 
 
 
-// Segment Tree Lazy Propagation
+// Segment Tree Lazy Propagation (Without Propagation Update)
 
 vector<pair<ull, ull> > tree;
 
@@ -109,5 +109,86 @@ ll query(ll pos, ll l, ll r, ll x, ll y, ll carry) {
 	ll rht = query(pos*2LL + 1, mid+1, r, x, y, carry + tree[pos].se);
 	
 	return lft + rht;
+}
+
+
+
+// Segment Tree Laze (With Propagation Update)
+
+struct node {
+    int val, prop;
+};
+
+// Prop :
+// 0 : No prop operation
+// 1 : Prop operation should be done
+
+node tree[409000];
+
+void init(int L, int R, int pos) {
+    if(L == R) {
+        tree[pos].val = 0;
+        tree[pos].prop = 0;
+        return;
+    }
+    
+    int mid = (L+R)>>1;
+    
+    init(L, mid, pos<<1);
+    init(mid+1, R, pos<<1|1);
+
+    tree[pos].val = 0;
+    tree[pos].prop = 0;
+}
+
+int flipProp(int parentVal, int childVal) {
+    if(parentVal == childVal)
+        return 0;
+    return parentVal;
+}
+        
+
+void propagate(int L, int R, int pos) {
+    if(tree[pos].prop == 0 || L == R)       // If no propagation tag
+        return;                             // or leaf node, then no need to change
+    
+    int mid = (L+R)>>1;
+    tree[pos<<1].val = (mid-L+1) - tree[pos<<1].val;        // Set left & right child value
+    tree[pos<<1|1].val = (R-mid) - tree[pos<<1|1].val;
+    
+    tree[pos<<1].prop = flipProp(tree[pos].prop, tree[pos<<1].prop);    // Flip child prop according to problem
+    tree[pos<<1|1].prop = flipProp(tree[pos].prop, tree[pos<<1|1].prop);
+    tree[pos].prop = 0;         // Clear parent propagation tag
+}
+
+void update(int L, int R, int l, int r, int pos) {
+    if(r < L || R < l)
+        return;
+    
+    propagate(L, R, pos);
+    if(l <= L && R <= r) {
+        tree[pos].val = (R-L+1) - tree[pos].val;    // Value updated
+        tree[pos].prop = 1;                         // Propagation tag set
+        return;
+    }
+    
+    int mid = (L+R)>>1;
+    update(L, mid, l, r, pos<<1);
+    update(mid+1, R, l, r, pos<<1|1);
+    tree[pos].val = tree[pos<<1].val + tree[pos<<1|1].val;
+}
+
+int querySum(int L, int R, int l, int r, int pos) {
+    if(r < L || R < l)
+        return 0;
+        
+    propagate(L, R, pos);
+    if(l <= L && R <= r)
+        return tree[pos].val;
+    
+    int mid = (L+R)>>1;
+    int lft = querySum(L, mid, l, r, pos<<1);
+    int rht = querySum(mid+1, R, l, r, pos<<1|1);
+    return lft+rht;
 }
 
