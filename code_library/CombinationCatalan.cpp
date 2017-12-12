e22abccb5d5f46ffe40f9f036acc6e447ddaa678
Quwsar Ohi <quwsarohi@gmail.com>
2017-12-12 09:06:32 +0600

    Create CombinationCatalan.cpp

---
+++ code_library/CombinationCatalan.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long C(int n, int k) { //Complexity O(k)
+    long long c = 1;
+    if(k > n - k)
+        k = n-k;
+    for(int i = 0; i < k; i++) {
+        c *= (n-i);
+        c /= (i+1);
+    }
+    return c;
+}
+
+long long catalan(int n) {    //Cat(n) = C(2*n, n)/(n+1);
+    long long c = C(2*n, n);
+    return c/(n+1);
+}
+
+int main() {}
