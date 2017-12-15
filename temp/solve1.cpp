25f60b6cbaf4711dfcf907f7962456649cbec45a
QuwsarOhi <quwsarohi@gmail.com>
2017-12-15 14:56:01 +0530

    new

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,89 +1,57 @@
-//  http://codeforces.com/contest/895/problem/C
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
-#include <iterator>
-#include <bitset>
-#include <vector>
-#include <math.h>
-#include <queue>
-#include <map>
-#include <set>
-#include <list>
-#include <time.h>
-#include <algorithm>
-#define mkp make_pair
-#define inf 1000000000
-#define MOD 1000000007
-#define eps 1e-7
-
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define ll long long
+#define pb push_back
+#define mem (a, b) memset (a, b, sizeof (a))
 
-int n;
-int mask[72];
-ll f[2][72];
-ll dp[2][1 << 20];
+const  int N = 1e5 + 5 ;
+const  int MOD = 1e9 + 7 ;
+int a[N];
+int cnt[75];
+int dp[75] [(1<<19)+5];
+int s[75];
+int prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67};
+int _2p [N];
 
-bool prime(int x)
-{
-	for (int i = 2; i*i <= x; i++)
-		if (x%i == 0)
-			return 0;
-	return 1;
-}
-
-void init()
-{
-	for (int i = 0; i < 72; i++)
-		f[0][i] = 1;
-	int cnt = 0;
-	for (int i = 2; i < 72; i++)
-	{
-		if (!prime(i))
-			continue;
-		for (int j = 1; j < 72; j++)
-		{
-			int x = j;
-			while (x%i == 0)
-			{
-				x /= i;
-				mask[j] ^= (1 << cnt);
-			}
-		}
-		cnt++;
-	}
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	init();
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		int x;
-		cin >> x;
-		f[0][x] = f[1][x] = (f[0][x] + f[1][x]) % MOD;
-	}
-	dp[0][0] = 1;
-	for (int i = 0; i <= 70; i++)
-	{
-		int nxt = (i + 1) % 2;
-		int cur = i % 2;
-		for (int msk = 0; msk < (1<<20); msk++)
-		{
-			dp[nxt][msk^mask[i]] = dp[nxt][msk^mask[i]] + dp[cur][msk] * f[1][i];
-			dp[nxt][msk] = dp[nxt][msk] + dp[cur][msk] * f[0][i];
-			if (dp[nxt][msk^mask[i]] >= MOD)
-				dp[nxt][msk^mask[i]] %= MOD;
-			if (dp[nxt][msk] >= MOD)
-				dp[nxt][msk] %= MOD;
-		}
-		for (int msk = 0; msk < (1<<20); msk++)
-			dp[cur][msk] = 0;
-	}
-	cout << (dp[1][0] - 1 + MOD)%MOD << endl;
-}
+int main () {
+    ios :: sync_with_stdio (false);
+    cin.tie (0);
+    
+    for (int i = 1; i<=70; i++) {
+        int t = i;
+        for(int j = 0; j<19; j++) {
+            while(t % prime[j] == 0) {
+                t /= prime[j];
+                s[i] ^ = (1 << j);
+            }
+        }
+    }
+    
+    _2p[0] = 1 ;
+    
+    for(int I=1; I < N; I++) 
+        _2p[I] = (_2p[I-1]*2 )%MOD;
+    
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+        cnt[a[i]]++ ;
+    }
+    dp[0][0] = 1;
+    
+    for (int i = 1; i <= 70; i++) {
+        if (!cnt[i]) {
+            for(int j = 0; j < (1<<19); j++)
+            dp[i][j] = dp[i-1][j];
+        }
+        else {
+            for ( int j = 0 ; j <( 1 << 19 ); j ++) {
+                dp [i] [j ^ s [i]] = ((ll) dp [i] [j ^ s [i]] + (ll) _2p [cnt [i] -1 ] * dp [i- 1 ] J]) the MOD%; // from cnt [i] a number selected from an odd number, C (n-,. 1) + C (n-,. 3) ^ 2 + ... = (. 1-n-) 
+                DP [I] [ j] = ((11) dp [i] [j] + (ll) _2p [cnt [i] -1 ] * dp [i- 1 ] [j])% MOD; // from cnt [i] C (n, 0) + C (n, 2) * C (n, 4) + ... = 2 ^ (n-1) 
+            }
+        }
+    }
+    cout << (dp [ 70 ] [ 0 ] - 1 )% MOD << endl; // Subtract 0 from case
+return  0 ;}
