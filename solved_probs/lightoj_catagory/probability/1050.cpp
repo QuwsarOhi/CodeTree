1c6ded465a4726d1a4131e08c5a04dfc24200043
Quwsar Ohi <quwsarohi@gmail.com>
2017-12-26 00:31:21 +0530

    Create 1050.cpp

---
+++ solved_probs/lightoj_catagory/probability/1050.cpp
@@ -0,0 +1,101 @@
+// LightOJ
+// 1050 - Marbles
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX                 200100
+#define EPS                 1e-9
+#define INF                 1e9+10
+#define MOD                 1000000007
+#define pb                  push_back
+#define mp                  make_pair
+#define fi                  first
+#define se                  second
+#define pi                  acos(-1)
+#define sf                  scanf
+#define pf                  printf
+#define SIZE(a)             ((int)a.size())
+#define Equal(a, b)         (abs(a-b) < EPS)
+#define Greater(a, b)       (a >= (b+EPS))
+#define GreaterEqual(a, b)  (a > (b-EPS)) 
+#define fr(i, a, b)         for(register int i = (a); i < (int)(b); i++)
+#define FastRead            ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define dbug(vari)          cerr << #vari << " = " << (vari) <<endl
+#define isOn(S, j)          (S & (1 << j))
+#define setBit(S, j)        (S |= (1 << j))
+#define clearBit(S, j)      (S &= ~(1 << j))
+#define toggleBit(S, j)     (S ^= (1 << j))
+#define lowBit(S)           (S & (-S))
+#define setAll(S, n)        (S = (1 << n) - 1)
+#define fileRead(S)         freopen(S, "r", stdin);
+#define fileWrite(S)        freopen(S, "w", stdout);
+#define Unique(X)           X.erase(unique(X.begin(), X.end()), X.end())
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef map<int, int> mii;
+typedef map<ll, ll>mll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<long long>vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pair<int, int> > vii;
+typedef vector<pair<ll, ll> >vll;
+
+double dp[510][510];
+
+double P(int r, int b) {
+    if(r != 0 && b == 0)
+        return dp[r][b] = 0;
+    if(r == 0 && b != 0)
+        return dp[r][b] = 1;
+    
+    if(dp[r][b] >= 0)
+        return dp[r][b];
+    
+    double t = r+b;
+    double takeRed = 0, takeBlue = 0;
+    
+    if(r > 0 && b > 0)
+        takeRed = r/t * P(r-1, b-1);
+    if(b >= 2)
+        takeBlue = b/t * P(r, b-2);
+    
+    dp[r][b] = takeRed + takeBlue;
+    return dp[r][b];
+}
+
+void PreCompute() {
+    for(int r = 0; r <= 500; ++r)
+        for(int b = 0; b <= 500; ++b) {
+            if(r == 0)
+                dp[r][b] = 1;
+            else if(b == 0)
+                dp[r][b] = 0;
+            else
+                dp[r][b] = P(r, b);
+        }
+}
+
+int main() {
+    //fileRead("in");
+    //fileWrite("out");
+    
+    int t, r, b;
+    sf("%d", &t);
+    
+    for(int i = 0; i <= 500; ++i)
+        for(int j = 0; j <= 500; ++j)
+            dp[i][j] = -1;
+    
+    PreCompute();
+    
+    for(int Case = 1; Case <= t; ++Case) {
+        sf("%d %d", &r, &b);
+        pf("Case %d: ", Case);
+        pf("%.6f\n", dp[r][b]);
+    }
+    
+    return 0;
+}
