5c1734fe0d25ecf62442e98579c583d4fc0d5e6f
quwsarohi <quwsarohi@gmail.com>
2018-01-01 16:30:48 +0600

    lst

--- temp/solve1.py
+++ temp/solve1.py
@@ -1,7 +1,7 @@
 import sys
 from bisect import *
 
-#sys.stdin = open('in', 'r')
+sys.stdin = open('in', 'r')
 #sys.stdout = open('out', 'w')
 
 
@@ -14,11 +14,29 @@ def In():
 Input = []
 def inputGrab():
     for line in sys.stdin:
-        Input.extend(map(int, line.strip().split()))
+        Input.extend(map(str, line.strip().split()))
 
 def main():
-    inputGrab()
+    #inputGrab()
     
+    Str = input()
+    b, s, c = map(int, input().strip().split())
+    pb, ps, pc = map(int, input().strip().split())
+    money = int(input())
+    
+    #print(s, type(s))
+    
+    bCnt = Str.count("B")
+    sCnt = Str.count("S")
+    cCnt = Str.count("C")
+    
+    ans = min(b//bCnt, s//sCnt, c//cCnt)
+    b -= ans*bCnt
+    s -= ans*sCnt
+    c -= ans*cCnt
+    
+    print(b, s, c)
+    print(ans)
     
     
     
