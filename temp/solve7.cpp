356e7ec121c59645b9ce404be03dde3c4d23209f
quwsarohi <quwsarohi@gmail.com>
2018-02-04 02:32:57 +0600

    sarr

--- temp/solve7.cpp
+++ temp/solve7.cpp
@@ -1,124 +1,55 @@
-// C++ program for building suffix array of a given text
-#include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
- 
-// Structure to store information of a suffix
-struct suffix
-{
-    int index; // To store original index
-    int rank[2]; // To store ranks and next rank pair
-};
- 
-// A comparison function used by sort() to compare two suffixes
-// Compares two pairs, returns 1 if first pair is smaller
-int cmp(struct suffix a, struct suffix b)
-{
-    return (a.rank[0] == b.rank[0])? (a.rank[1] < b.rank[1] ?1: 0):
-               (a.rank[0] < b.rank[0] ?1: 0);
+#define MAX                 200100
+#define EPS                 1e-9
+#define INF                 1e7
+#define MOD                 1000000007
+#define pb                  push_back
+#define mp                  make_pair
+#define fi                  first
+#define se                  second
+#define pi                  acos(-1)
+#define sf                  scanf
+#define pf                  printf
+#define SIZE(a)             ((int)a.size())
+#define All(S)              S.begin(), S.end()              
+#define Equal(a, b)         (abs(a-b) < EPS)
+#define Greater(a, b)       (a >= (b+EPS))
+#define GreaterEqual(a, b)  (a > (b-EPS))
+#define fr(i, a, b)         for(register int i = (a); i < (int)(b); i++)
+#define FastRead            ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define fileRead(S)         freopen(S, "r", stdin);
+#define fileWrite(S)        freopen(S, "w", stdout);
+#define Unique(X)           X.erase(unique(X.begin(), X.end()), X.end())
+#define error(args...)      { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+#define isOn(S, j)          (S & (1 << j))
+#define setBit(S, j)        (S |= (1 << j))
+#define clearBit(S, j)      (S &= ~(1 << j))
+#define toggleBit(S, j)     (S ^= (1 << j))
+#define lowBit(S)           (S & (-S))
+#define setAll(S, n)        (S = (1 << n) - 1)
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef map<int, int> mii;
+typedef map<ll, ll>mll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<long long>vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pair<int, int> > vii;
+typedef vector<pair<ll, ll> >vll;
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {                                                  // Debugger error(a, b, ....)
+	cerr << *it << " = " << a << "\n";
+	err(++it, args...);
 }
 
-int order(char x) {
-    if(isdigit(x))
-        return x-'0';
-    else if(isupper(x))
-        return x-'A'+11;
-    else
-        return x-'a'+38;
-}
+//const int dx[4][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};                                                      // Four side 
+//const int dxx[8][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};                     // Eight side
+//----------------------------------------------------------------------------------------------------------
 
- 
-// This is the main function that takes a string 'txt' of size n as an
-// argument, builds and return the suffix array for the given string
-int *buildSuffixArray(char *txt, int n)
-{
-    // A structure to store suffixes and their indexes
-    struct suffix suffixes[n];
- 
-    // Store suffixes and their indexes in an array of structures.
-    // The structure is needed to sort the suffixes alphabatically
-    // and maintain their old indexes while sorting
-    for (int i = 0; i < n; i++)
-    {
-        suffixes[i].index = i;
-        suffixes[i].rank[0] = order(txt[i]);
-        suffixes[i].rank[1] = ((i+1) < n)? order(txt[j]): -1;
-    }
- 
-    // Sort the suffixes using the comparison function
-    // defined above.
-    sort(suffixes, suffixes+n, cmp);
- 
-    // At his point, all suffixes are sorted according to first
-    // 2 characters.  Let us sort suffixes according to first 4
-    // characters, then first 8 and so on
-    int ind[n];  // This array is needed to get the index in suffixes[]
-                 // from original index.  This mapping is needed to get
-                 // next suffix.
-    for (int k = 4; k < 2*n; k = k*2)
-    {
-        // Assigning rank and index values to first suffix
-        int rank = 0;
-        int prev_rank = suffixes[0].rank[0];
-        suffixes[0].rank[0] = rank;
-        ind[suffixes[0].index] = 0;
- 
-        // Assigning rank to suffixes
-        for (int i = 1; i < n; i++)
-        {
-            // If first rank and next ranks are same as that of previous
-            // suffix in array, assign the same new rank to this suffix
-            if (suffixes[i].rank[0] == prev_rank &&
-                    suffixes[i].rank[1] == suffixes[i-1].rank[1])
-            {
-                prev_rank = suffixes[i].rank[0];
-                suffixes[i].rank[0] = rank;
-            }
-            else // Otherwise increment rank and assign
-            {
-                prev_rank = suffixes[i].rank[0];
-                suffixes[i].rank[0] = ++rank;
-            }
-            ind[suffixes[i].index] = i;
-        }
- 
-        // Assign next rank to every suffix
-        for (int i = 0; i < n; i++)
-        {
-            int nextindex = suffixes[i].index + k/2;
-            suffixes[i].rank[1] = (nextindex < n)?
-                                  suffixes[ind[nextindex]].rank[0]: -1;
-        }
- 
-        // Sort the suffixes according to first k characters
-        sort(suffixes, suffixes+n, cmp);
-    }
- 
-    // Store indexes of all sorted suffixes in the suffix array
-    int *suffixArr = new int[n];
-    for (int i = 0; i < n; i++)
-        suffixArr[i] = suffixes[i].index;
- 
-    // Return the suffix array
-    return  suffixArr;
-}
- 
-// A utility function to print an array of given size
-void printArr(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
- 
-// Driver program to test above functions
-int main()
-{
-    char txt[] = "abracadabra0AbRa4Cad14abra";
-    int n = strlen(txt);
-    int *suffixArr = buildSuffixArray(txt,  n);
-    cout << "Following is suffix array for " << txt << endl;
-    printArr(suffixArr, n);
-    return 0;
-}
