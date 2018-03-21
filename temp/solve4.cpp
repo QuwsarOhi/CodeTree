2844589565a090e451e7e4c3a9684d418ab78307
quwsarohi <quwsarohi@gmail.com>
2018-03-22 04:24:42 +0600

    cf

--- temp/solve4.cpp
+++ temp/solve4.cpp
@@ -1,1 +1,56 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// p = 31, 51
+// MOD = 1e9+9, 1e7+7
+
+
+// Returns Single Hash Val
+ll hash(char *s,  int len, ll mod = 1e9+9) {
+    int p = 31;
+    ll hashVal = 0;
+    ll pPow = 1;
+    
+    for(int i = 0; i < len; ++i) {
+        hashVal = (hashVal + (s[i] - 'a' + 1) * pPow)%mod;
+        pPow = (pPow *p)%mod;
+    }
+    return hashVal;
+}
+
+
+// Generates Continious Hash Val
+vector<ll> ContHash(char *s, int len, ll mod = 1e9+9) {
+    int p = 31;
+    ll hashVal = 0;
+    ll pPow = 1;
+    vector<ll>v;
+    
+    for(int i = 0; i < len; ++i) {
+        hashVal = (hashVal + (s[i] - 'a' + 1) * pPow)%mod;
+        pPow = (pPow *p)%mod;
+        v.push_back(hashVal);
+    }
+    
+    return v;
+}
+
+
+vector<pair<ll, ll> > doubleHash(char *s, int len, ll mod1 = 1e9+9, ll mod2 = 1e9+7) {
+    int p = 31;
+    ll hashVal1 = 0, hashVal2 = 0;
+    ll pPow1 = 1, pPow2 = 1;
+    vector<pair<ll, ll> >v;
+    
+    for(int i = 0; i < len; ++i) {
+        hashVal1 = (hashVal1 + (s[i] - 'a' + 1)* pPow1)%mod1;
+        hashVal2 = (hashVal2 + (s[i] - 'a' + 1)* pPow2)%mod2;
+        pPow1 = (pPow1 * p)%mod1;
+        pPow2 = (pPow2 * p)%mod2;
+        v.push_back({hashVal1, hashVal2});
+    }
+    
+    return v;
+}
--- temp/solve5.cpp
+++ temp/solve5.cpp
@@ -2,33 +2,91 @@
 using namespace std;
 typedef long long ll;
 
-ll f[50];
-vector<ll>ans;
+// p = 31, 51
+// MOD = 1e9+9, 1e9+7
 
-void F() {
-    f[1] = 0;
-    f[2] = 1;
-    f[3] = 2;
-    for(int i = 4; i < 50; ++i)
-        f[i] = f[i-1] + f[i-2];
+pair<ll, ll> doubleHash(string s, ll mod1 = 1e9+9, ll mod2 = 1e9+7) {
+    int p = 31;
+    ll hashVal1 = 0, hashVal2 = 0;
+    ll pPow1 = 1, pPow2 = 1;
+    //vector<pair<ll, ll> >v;
+    
+    for(int i = 0; i < (int)s.size(); ++i) {
+        hashVal1 = (hashVal1 + (s[i] - 'a' + 1)* pPow1)%mod1;
+        hashVal2 = (hashVal2 + (s[i] - 'a' + 1)* pPow2)%mod2;
+        pPow1 = (pPow1 * p)%mod1;
+        pPow2 = (pPow2 * p)%mod2;
+        //v.push_back({hashVal1, hashVal2});
+    }
+    return {hashVal1, hashVal2};
 }
 
+
+vector<pair<ll, ll> >ContDoubleHash(string s, ll mod1 = 1e9+9, ll mod2 = 1e9+7) {
+    int p = 31;
+    ll hashVal1 = 0, hashVal2 = 0;
+    ll pPow1 = 1, pPow2 = 1;
+    vector<pair<ll, ll> >v;
+    
+    for(int i = 0; i < (int)s.size(); ++i) {
+        hashVal1 = (hashVal1 + (s[i] - 'a' + 1)* pPow1)%mod1;
+        hashVal2 = (hashVal2 + (s[i] - 'a' + 1)* pPow2)%mod2;
+        pPow1 = (pPow1 * p)%mod1;
+        pPow2 = (pPow2 * p)%mod2;
+        v.push_back({hashVal1, hashVal2});
+    }
+    return v;
+}
+
+vector<pair<ll, ll> >Hash;
+pair<ll, ll>Patt;
+string s;
+const int p = 31;
+const ll mod1 = 1e9+9, mod2 = 1e9+7;
+
+
 int main() {
-    ll s, k;
+    //freopen("in", "r", stdin);
     
-    cin >> s >> k;
-    F();
+    ll n;
     
-    for(int i = 49; i > 0; --i)
-        if(f[i] <= s && s >= 0) {
-            ans.push_back(f[i]);
-            s -= f[i];
+    while(scanf("%lld ", &n) == 1) {
+        getline(cin, s);
+        Patt = doubleHash(s, mod1, mod2);
+        
+        //cout << Patt.first << " " << Patt.second << endl;
+        
+        getline(cin, s);
+        
+        Hash = ContDoubleHash(s, mod1, mod2);
+        bool ok = 0;
+        
+        /*for(int i = 0; i < n-1; ++i) {
+            Patt.first = (Patt.first * p)%mod1;
+            Patt.second = (Patt.second * p)%mod2;
+        }*/
+        
+        
+        for(int i = n-1; i < int(Hash.size()); ++i) {
+            pair<ll, ll> RangeHash = Hash[i];
+            
+            if(i-n >= 0) {
+                RangeHash.first = (RangeHash.first + mod1 - Hash[i-n].first)%mod1;
+                RangeHash.second = (RangeHash.second + mod2 - Hash[i-n].second)%mod2;
+            }
+            
+            if(Patt == RangeHash) {
+                ok = 1;
+                cout << i-n+1 << "\n";
+            }
+            
+            //cout << i << " :: " << Patt.first << " " << RangeHash.first << endl;
+            
+            Patt.first = (Patt.first * p)%mod1;
+            Patt.second = (Patt.second * p)%mod2;
         }
-    
-    cout << ans.size() << endl;
-    for(auto it : ans)
-        cout << it << " ";
-    cout << endl;
-    
+        
+        if(!ok) cout << "\n";
+    }
     return 0;
 }
