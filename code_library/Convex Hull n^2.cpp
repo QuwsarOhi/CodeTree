c1080329e0e5628cd303b692d9342c78d53d94dc
Quwsar Ohi <quwsarohi@gmail.com>
2018-05-05 00:25:38 +0600

    Update Convex Hull n^2.cpp

--- code_library/Convex Hull n^2.cpp
+++ code_library/Convex Hull n^2.cpp
@@ -1,43 +1,28 @@
 // Convex Hull
 // Gift Wrapping (Jarvis)
 // Complexity : O(n^2)
-
 // https://www.geeksforgeeks.org/convex-hull-set-1-jarviss-algorithm-or-wrapping/
 // https://www.hackerearth.com/problem/approximate/polygon-partition-910a3b76/
-
-#include <bits/stdc++.h>
-#define x first
-#define y second
-using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pll;
-
 
 ll whichSide(pll p, pll q, pll r) {     // returns on which side point r is w.r.t pq line
     // slope = 0 : linear
     // slope > 0 : right
     // slope < 0 : left
-    
     ll slope = (p.y-q.y)*(q.x-r.x) - (q.y-r.y)*(p.x-q.x);
     return slope;
 }
 
-
 ll dist(pll a, pll b) {
     ll X = a.x - b.x;
     ll Y = a.y - b.y;
-    
     return X*X + Y*Y;
 }
 
 
 vector<int> hull;       // Used in convexHull
-
 bool convexHull(vector<pll> &v) {
     if(v.size() < 3)
         return 0;
-    
-    
     
     // Finding Upper LeftMost point
     int l = 0;
@@ -47,12 +32,10 @@ bool convexHull(vector<pll> &v) {
     
     //printf("Starting %lld %lld\n", v[l].x, v[l].y);
     int p = l, q;
-    
     do {
         hull.push_back(p);          // Hull point index is pushed
         
         q = (p+1) % v.size();
-        
         // This checks if there exists same point twice (no need if points are unique)
         while(v[p] == v[q])
             q = (q+1) % v.size();
@@ -61,7 +44,6 @@ bool convexHull(vector<pll> &v) {
             ll side = whichSide(v[p], v[q], v[i]);
             if(side < 0)                                                        // point i is on left w.r.t point pq
                 q = i;
-            
             // Use this commented code, if minimum number of points needed (points in same line is rejected)
             //else if(side == 0 and dist(v[p], v[q]) < dist(v[p], v[i]))          // if point i is on same line pq
             //    q = i;
@@ -91,11 +73,8 @@ int main() {
     sz = 0;
     
     while(1) {
-        if(!convexHull(points)) {
+        if(!convexHull(points))
             break;
-        }
-        
-        //cout << "WORKING\n";
         sort(hull.begin(), hull.end(), greater<int>());
         
         for(auto idx : hull) {
@@ -107,20 +86,17 @@ int main() {
         hull.clear();
         ++sz;
     }
-    
-    
-    
+
     for(int i = 0; i < (int)points.size(); ++i)
         Ans[sz].push_back(Map[points[i]].back());
     sz += (points.empty() == 0);
-        
+
     printf("%d\n", sz);
     for(int i = 0; i < sz; ++i) {
         printf("%d", Ans[i].size());
         for(auto it : Ans[i])
             printf(" %d", it+1);
         printf("\n");
-    }
-        
+    }      
     return 0;
 }
