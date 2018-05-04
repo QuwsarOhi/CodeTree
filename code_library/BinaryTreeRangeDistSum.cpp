c471ac9205242615fb006c029c0672dd42afb66c
Quwsar Ohi <quwsarohi@gmail.com>
2018-05-05 00:23:23 +0600

    Update BinaryTreeRangeDistSum.cpp

--- code_library/BinaryTreeRangeDistSum.cpp
+++ code_library/BinaryTreeRangeDistSum.cpp
@@ -1,41 +1,6 @@
-// http://mishadoff.com/blog/dfs-on-binary-tree-array/
 // Complete Binary Tree
 // Sum of distance from a node "n" such that every nodes distance from node "n" is less than or equal to k
-// BinaryTreeRangeDistSum
-
-#include <bits/stdc++.h>
-using namespace std;
-#define EPS 1e-9
-#define INF 1e9+10
-#define MOD 1000000007
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
-#define pi acos(-1)
-#define sf scanf
-#define pf printf
-#define SIZE(a) ((int)a.size())
-#define Equal(a, b) (abs(a-b) < EPS)
-#define Greater(a, b) (a >= (b+EPS))
-#define GreaterEqual(a, b) (a > (b-EPS)) 
-#define fr(i, a, b) for(register int i = (a); i < (int)(b); i++)
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define dbug(vari) cerr << #vari << " = " << (vari) <<endl
-#define isOn(S, j) (S & (1 << j))
-#define setBit(S, j) (S |= (1 << j))
-#define clearBit(S, j) (S &= ~(1 << j))
-#define toggleBit(S, j) (S ^= (1 << j))
-#define lowBit(S) (S & (-S))
-#define setAll(S, n) (S = (1 << n) - 1)
-typedef unsigned long long ull;
-typedef long long ll;
-typedef map<int, int> mii;
-typedef map<string, int> msi;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pair<int, int> > vii;
+// http://mishadoff.com/blog/dfs-on-binary-tree-array/
 
 vector<ll>v[2000100], w, sum[2000100];
 ll n, m;
@@ -53,7 +18,6 @@ void dfs(ll node) {
     v[node][0] = 0;                             // distance from this node to this node
     
     //printf("node : %d, nodeSize : %d, lftSize : %d, rhtSize : %d\n", node, nodeSize, lftSize, rhtSize);
-    
     ll l = 0, r = 0;
     for(ll i = 1; i < nodeSize; ++i) {
         if(l == lftSize)
@@ -69,11 +33,7 @@ void dfs(ll node) {
             else {
                 v[node][i] = rhtW;
                 r++;
-            }
-        }
-    }
-}
-
+}}}}
 
 ll single(int node, ll d, ll delta) {
     if(d < 0) return 0;
@@ -95,21 +55,11 @@ ll query(int node, ll k) {
 }
 
 int main() {
-    fastIO;
     cin >> n >> m;                   // n = total nodes, m = number of query
     w.resize(n+5);
-    
     for(int i = 2; i <= n; ++i)     // Weight of tree w[x] contains distance from upper node to x (complete binary tree)
         cin >> w[i];
-    
     dfs(1);
-    
-    /*for(int i = 1; i <= n; ++i) {
-        cout << i << " ::  ";
-        for(int j = 0; j < SIZE(v[i]); ++j)
-            cout << v[i][j] << " ";
-        cout <<  endl;
-    }*/
     
     for(int i = 1; i <= n; ++i) {
         sum[i].resize(v[i].size());
@@ -117,7 +67,6 @@ int main() {
         for(int j = 1; j < SIZE(v[i]); ++j)
             sum[i][j] = sum[i][j-1] + v[i][j];
     }
-    
     while(m--) {
         ll node, k;
         cin >> node >> k;
