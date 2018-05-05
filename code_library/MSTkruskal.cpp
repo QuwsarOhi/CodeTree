056cdb1351cd181628e7a37daea634178b17de07
Quwsar Ohi <quwsarohi@gmail.com>
2018-05-05 11:59:44 +0600

    Update MSTkruskal.cpp

--- code_library/MSTkruskal.cpp
+++ code_library/MSTkruskal.cpp
@@ -1,56 +1,6 @@
-#include <bits/stdc++.h>
-#define MAX 1000
-
-using namespace std;
 
 vector<pair<int, pair<int, int> > > Edge;
 vector<int>u_list, u_set;
-
-//u_list[x] contains the size of a set x
-//u_set[x] contains the root of x
-
-int unionRoot(int n)
-{
-    if(u_set[n] == n)
-        return n;
-    else
-        return u_set[n] = unionRoot(u_set[n]);
-}
-
-int makeUnion(int a, int b)
-{
-    int x = unionRoot(a);
-    int y = unionRoot(b);
-
-    if(x == y)
-        return x;
-    else if(u_list[x] > u_list[y]) {
-        u_set[y] = x;
-        u_list[x] += u_list[y];
-        return x;
-    }
-    else {
-        u_set[x] = y;
-        u_list[y] += u_list[x];
-        return y;
-    }
-}
-
-void unionInit(int len)
-{
-    u_list.resize(len+5, 1);
-    u_set.resize(len+5);
-
-    for(int i = 0; i <= len; i++)
-        u_set[i] = i;
-}
-
-bool isSameSet(int a, int b)
-{
-    if(unionRoot(a) == unionRoot(b))
-        return 1;
-    return 0;
-}
 
 
 int main()
@@ -76,8 +26,7 @@ int main()
 			selected_edge++;
 			mst_cost += w;
 			makeUnion(u, v);
-		}
-	}
+		}}
 	
 	printf("MST in Kruskal : %d\n", mst_cost);
 	Edge.clear();
