5f4b33661528e58d99ce5fd6ebb00a75bd030d99
quwsarohi <quwsarohi@gmail.com>
2018-05-09 19:21:49 +0600

    failure

--- temp/solve3.cpp
+++ temp/solve3.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 string g[102];
 int cnt = 0, n, TOT;
-bool p[102][102];
+bool p[102][102], vis[102][102];
 
 vector<pair<int, int> > Ans, V;
 
 void SET(int x, int y, bool v) {
+    vis[x][y] = v;
     for(int i = 0; i < n; ++i) {
         if(p[x][i] != v) {
-            if(v) cnt += 1;
+            if(v) cnt += 1; //cout << "ADD " << x << " " << i << endl; }
             else cnt -= 1;
             p[x][i] = v;
         }
         if(p[i][y] != v) {
-            if(v) cnt += 1;
+            if(v) cnt += 1; //cout << "ADD " << i << " " << y << endl; }
             else cnt -= 1;
             p[i][y] = v;
         }
@@ -24,7 +25,10 @@ void SET(int x, int y, bool v) {
 
 
 void recur(int x, int y) {
-    cout << x << " " << y << " " << cnt << endl;
+    cout << x+1 << " " << y+1 << " " << cnt << endl;
+    
+    V.push_back({x, y});
+    SET(x, y, 1);
      
     if(cnt == TOT) {
         cout << "ANS SIZE " << V.size() << endl;
@@ -36,19 +40,16 @@ void recur(int x, int y) {
     if(not Ans.empty() and Ans.size() < V.size())
         return;
     
-    V.push_back({x, y});
-    SET(x, y, 1);
-    
     for(int i = 0; i < n; ++i)
         for(int j = 0; j < n; ++j)
-            if(!p[i][j] and g[i][j] != 'E')
+            if(not vis[i][j] and g[i][j] != 'E')
                 recur(i, j);
     
     V.pop_back();
     SET(x, y, 0);
     
     cout << "Fall\n";
-    getchar();
+    //getchar();
 }
 
 int main() {
@@ -61,11 +62,14 @@ int main() {
     for(int i = 0; i < n; ++i)
         for(int j = 0; j < n; ++j)
             if(g[i][j] != 'E') {
-                cout << "NEW START " << i << " " << j << endl;
+                //cout << "NEW START " << i << " " << j << endl;
                 recur(i, j);
             }
     
+    if(Ans.empty())
+        return !puts("-1");
+    
     for(auto it : Ans)
-        cout << it.first << " " << it.second << endl;
+        cout << it.first+1 << " " << it.second+1 << endl;
     return 0;
 }
