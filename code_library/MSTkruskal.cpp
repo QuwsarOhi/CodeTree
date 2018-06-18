264d79472034dae5ca7f9a60de11df2107f2f81d
quwsarohi <quwsarohi@gmail.com>
2018-06-19 02:00:06 +0600

    DSU

--- code_library/MSTkruskal.cpp
+++ code_library/MSTkruskal.cpp
@@ -1,74 +1,65 @@
-// MST Kruskal + Union Find Disjoint Set
+// MST Kruskal + Union Find Disjoint Set (DSU)
 // Complexity of MST : O(E logV)
 
-vector<int>u_list, u_set;               // u_list[x] contains the size of a set x
-                                        // u_set[x] contains the root of x
-int unionRoot(int n) {
-    if(u_set[n] == n)
-        return n;
-    else
+struct DSU {
+    vector<int>u_list, u_set;        // u_list[x] : the size of a set x,  u_set[x] : the root of x
+    
+    DSU() {}
+    DSU(int SZ) { init(SZ); }
+    
+    int unionRoot(int n) {
+        if(u_set[n] == n) return n;
         return u_set[n] = unionRoot(u_set[n]);
-}
-
-int makeUnion(int a, int b) {
-    int x = unionRoot(a);
-    int y = unionRoot(b);
-    if(x == y)
-        return x;
-    else if(u_list[x] > u_list[y]) {
-        u_set[y] = x;
-        u_list[x] += u_list[y];
-        return x;
     }
-    else {
-        u_set[x] = y;
-        u_list[y] += u_list[x];
-        return y;
+
+    int makeUnion(int a, int b) {
+        int x = unionRoot(a), y = unionRoot(b);
+        if(x == y)
+            return x;
+        else if(u_list[x] > u_list[y]) {
+            u_set[y] = x;
+            u_list[x] += u_list[y];
+            return x;
+        }
+        else {
+            u_set[x] = y;
+            u_list[y] += u_list[x];
+            return y;
+    }}
+
+    void init(int len) {
+        u_list.resize(len+5);
+        u_set.resize(len+5);
+        for(int i = 0; i <= len+3; i++)
+            u_set[i] = i, u_list[i] = 0;
     }
-}
 
-void unionInit(int len) {
-    u_list.resize(len+5, 1);
-    u_set.resize(len+5);
-    for(int i = 0; i <= len+3; i++)
-        u_set[i] = i, u_list[i] = 0;
-}
-
-bool isSameSet(int a, int b) {
-    if(unionRoot(a) == unionRoot(b))
-        return 1;
-    return 0;
-}
+    bool isSameSet(int a, int b) {
+        return (unionRoot(a) == unionRoot(b));
+    }
+};
 
 // Let a graph be G1, and the MST of the graph is MST1
 // and a graph G2, where G2 contains same edges as G1 with some new edges
 // then the new MST of graph G2 will be :
 // MST2 = MST(of the edges used in M1 (MST of G1) + new added edges)
 
-vector<pair<int, pair<int, int> > > Edge;               // If edges are inserted frequently, use STL SET
+set<pair<int, pair<int, int> > >Edge;                   // USED STL SET!!
 
 int MST(int V) {
-    sort(Edge.begin(), Edge.end());                     // Not needed if C++ SET is used
-	int mst_cost = 0, selected_edge = 0;                // There exists no MST if, Selected Edges < V-1
-	unionInit(V);
-	for(int i = 0; i < (int)Edge.size() && selected_edge < V; i++) {
-		u = Edge[i].second.first;
-		v = Edge[i].second.second;
-		w = Edge[i].first;
-		if(!isSameSet(u, v))
-			selected_edge++, mst_cost += w, makeUnion(u, v);
-    }}
-    return mst_cost;
-}
-
-int main() {
-	int V, E, u, v, w;
-	scanf("%d %d", &V, &E);
-	for(int i = 0; i < E; i++) {
-		scanf("%d %d %d", &u, &v, &w);
-		Edge.push_back(make_pair(w, make_pair(u, v)));
-	}
-	printf("MST in Kruskal : %d\n", MST(V));
-	Edge.clear();
-	return 0;
+    int mstCost = 0, edge = 0;                          // If Edge list is STL vector, then sort it!
+    DSU U(V+5);
+    set<pair<int, pair<int, int> > > :: iterator it = Edge.begin();     // Contains {Weight, {U, V}}
+    
+    for( ; it != Edge.end() && edge < V; ++it) {
+        int u = (*it).second.first;
+        int v = (*it).second.second;
+        int w = (*it).first;
+        
+        if(!U.isSameSet(u, v))
+            ++edge, mstCost += w, U.makeUnion(u, v);
+    }
+    
+    if(edge != V-1) return -1;              // Some edge is missing, so no MST found!
+    return mstCost;
 }
--- temp/solve4.cpp
+++ temp/solve4.cpp
@@ -4,51 +4,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int>u_list, u_set;               // u_list[x] contains the size of a set x
-                                        // u_set[x] contains the root of x
+
 set<pair<int, pair<int, int> > >Edge;
 int E[201][201];
 
-int unionRoot(int n) {
-    if(u_set[n] == n)
-        return n;
-    else
+struct DSU {
+    vector<int>u_list, u_set;        // u_list[x] : the size of a set x,  u_set[x] : the root of x
+    
+    DSU() {}
+    DSU(int SZ) { init(SZ); }
+    
+    int unionRoot(int n) {
+        if(u_set[n] == n) return n;
         return u_set[n] = unionRoot(u_set[n]);
-}
-
-int makeUnion(int a, int b) {
-    int x = unionRoot(a);
-    int y = unionRoot(b);
-    if(x == y)
-        return x;
-    else if(u_list[x] > u_list[y]) {
-        u_set[y] = x;
-        u_list[x] += u_list[y];
-        return x;
     }
-    else {
-        u_set[x] = y;
-        u_list[y] += u_list[x];
-        return y;
+
+    int makeUnion(int a, int b) {
+        int x = unionRoot(a), y = unionRoot(b);
+        if(x == y)
+            return x;
+        else if(u_list[x] > u_list[y]) {
+            u_set[y] = x;
+            u_list[x] += u_list[y];
+            return x;
+        }
+        else {
+            u_set[x] = y;
+            u_list[y] += u_list[x];
+            return y;
+    }}
+
+    void init(int len) {
+        u_list.resize(len+5);
+        u_set.resize(len+5);
+        for(int i = 0; i <= len+3; i++)
+            u_set[i] = i, u_list[i] = 0;
     }
-}
 
-void unionInit(int len) {
-    u_list.resize(len+5, 1);
-    u_set.resize(len+5);
-    for(int i = 0; i <= len+3; i++)
-        u_set[i] = i, u_list[i] = 0;
-}
-
-bool isSameSet(int a, int b) {
-    if(unionRoot(a) == unionRoot(b))
-        return 1;
-    return 0;
-}
+    bool isSameSet(int a, int b) {
+        return (unionRoot(a) == unionRoot(b));
+    }
+};
 
 int MST(int V) {
     int mstCost = 0, edge = 0;
-    unionInit(V+10);
+    DSU U(V+5);
     set<pair<int, pair<int, int> > > :: iterator it = Edge.begin();
     
     for( ; it != Edge.end() && edge < V; ++it) {
@@ -56,9 +56,9 @@ int MST(int V) {
         int v = (*it).second.second;
         int w = (*it).first;
         
-        if(!isSameSet(u, v)) {
+        if(!U.isSameSet(u, v)) {
             E[u][v] = E[v][u] = w;
-            ++edge, mstCost += w, makeUnion(u, v);
+            ++edge, mstCost += w, U.makeUnion(u, v);
         }
     }
     
