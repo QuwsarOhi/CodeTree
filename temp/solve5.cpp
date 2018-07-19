fbfd7c3eb5d4b2ea8bd108b92fc4a771b3fa8006
quwsarohi <quwsarohi@gmail.com>
2018-07-19 15:53:24 +0600

    ST

--- temp/solve5.cpp
+++ temp/solve5.cpp
@@ -2,9 +2,33 @@
 using namespace std;
 typedef long long ll;
 
-ll dp[20][11];
 
-ll recur(int pos, int on, ll mask) {
+int main() {
+    int t, n, m, a, b;
+    scanf("%d", &t);
+    vector<ll>v;
     
-    
-    
+    for(int Case = 1; Case <= t; ++Case) {
+        scanf("%d%d%d%d", &n, &m, &a, &b);
+        
+        v.clear();
+        for(int i = 0; i < n; ++i)
+            v.push_back(1);
+        for(int i = 0; i < m; ++i)
+            v.push_back(0);
+        
+        ll sum = 0, ret = -1;
+        do {
+            sum = 0;
+            for(auto it : v)
+                sum = (sum<<1) + it;
+            if(sum <= b and a <= sum) {
+                ret = sum;
+                break;
+            }
+        } while(prev_permutation(v.begin(), v.end()));
+        
+        printf("Case %d: %lld\n", Case, ret);
+    }
+    return 0;
+}
--- temp/solve6.cpp
+++ temp/solve6.cpp
@@ -1,1 +1,94 @@
+#include <bits/stdc++.h>
+#define MAX 100
+using namespace std;
+typedef long long ll;
 
+struct STree {
+    ll sum[4*MAX], prop[4*MAX], leftProp[4*MAX], rightProp[4*MAX];
+    
+    void propagate0(int pos, int l, int r) {
+        if(prop[pos] == 0) return;
+        if(l == r) return;
+        int mid = (l+r)>>1;
+        
+        sum[pos<<1] += prop[pos]*(mid-l+1);
+        sum[pos<<1|1] += prop[pos]*(r-mid);
+        prop[pos<<1] = prop[pos<<1|1] = prop[pos];
+        prop[pos] = 0;
+    }
+    
+    void update0(int pos, int l, int r, int L, int R, int val) {    // update val, val, val, ... at each index 
+        if(r < L || R < l) return;
+        propagate0(pos, l, r);
+        if(L <= l && r <= R) {
+            sum[pos] += val*(r-l+1);
+            prop[pos] += pos;
+            return;
+        }
+        int mid = (l+r)>>1;
+        update0(pos<<1, l, mid, L, R, val), update0(pos<<1|1, mid+1, r, L, R, val);
+        sum[pos] = sum[pos<<1] + sum[pos<<1|1];
+    }
+    
+    void propagate1(int pos, int l, int r) {                    // propagate func for update1
+        if(leftProp[pos] == 0) return;
+        if(l == r) return;
+        int mid = (l+r)>>1;
+        
+        sum[pos<<1] += getSeqSumLeft(leftProp[pos], mid-l+1);
+        sum[pos<<1|1] += getSeqSumLeft(mid+2-leftProp[pos], mid-r);
+        leftProp[pos<<1] = leftProp[pos<<1|1] = leftProp[pos];
+        leftProp[pos] = 0;
+    }
+    
+    void update1(int pos, int l, int r, int L, int R) {         // update like 1, 2, 3, ... at each index i
+        if(r < L || R < l) return;
+        propagate1(pos, l, r);
+        if(L <= l && r <= R) {
+            sum[pos] += getSeqSumLeft(l-L+1, r-l+1);
+            leftProp[pos] = L;
+            return;
+        }
+        int mid = (l+r)>>1;
+        update1(pos<<1, l, mid, L, R), update1(pos<<1|1, mid+1, L, R);
+        sum[pos] = sum[pos<<1] + sum[pos<<1|1];
+    }
+    
+    void propagate2(int pos, int l, int r) {
+        if(rightProp[pos] == 0) return;
+        if(l == r) return;
+        int mid = (l+r)>>1;
+        
+        sum[pos<<1] += getSeqSumRight(mid-rightProp[pos]+1, mid-l+1);
+        sum[pos<<1|1] += getSeqSumRight(r-rightProp[pos]+1, r-mid);
+        rightProp[pos<<1] = rightProp[pos<<1|1] = rightProp[pos];
+        rightProp[pos] = 0;
+    }
+    
+    void update2(int pos, int l, int r, int L, int R) {                       // update like 3, 2, 1 at each index i
+        if(r < L || R < l) return;
+        propagate2(pos, l, r);
+        if(L <= l && r <= R) {
+            sum[pos] += getSeqSumRight(R-l+1, r-l+1);
+            rightProp[pos] = R;
+            return;
+        }
+        int mid = (l+r)>>1;
+        update2(pos<<1, l, mid, L, R), update2(pos<<1|1, mid+1, r, L, R);
+        sum[pos] = sum[pos<<1] + sum[pos<<1|1];
+    }
+     
+     
+    ll query(int pos, int l, int r, int L, int R) {
+        propagate0(pos, l, r), propagate1(pos, l, r), propagate2(pos, l, r);
+        if(L <= l && r <= R) return sum[pos];
+        int mid = (l+r)>>1;
+        return query(pos<<1, l, mid, L, R) + query(pos<<1|1, mid+1, r, L, R);
+    }
+};
+
+
+int main() {
+    return 0;
+}
+        
