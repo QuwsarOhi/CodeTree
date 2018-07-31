7bbfd4216b770412197b60061f2764806f057924
Quwsar Ohi <quwsarohi@gmail.com>
2018-08-01 02:15:34 +0600

    Update Hash.cpp

--- code_library/Hash.cpp
+++ code_library/Hash.cpp
@@ -1,13 +1,6 @@
 // Hashing
-
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pll;
-
 // p = 31, 51
 // MOD = 1e9+9, 1e7+7
-
 const ll p = 31;
 const ll mod1 = 1e9+9, mod2 = 1e9+7;
 
@@ -16,25 +9,21 @@ ll hash(char *s,  int len, ll mod = 1e9+9) {
     int p = 31;
     ll hashVal = 0;
     ll pPow = 1;
-    
     for(int i = 0; i < len; ++i) {
         hashVal = (hashVal + (s[i] - 'a' + 1) * pPow)%mod;
         pPow = (pPow *p)%mod;
     }
     return hashVal;
 }
-
 vl Hash(char *s, int len) {
     ll hashVal = 0;
     vector<ll>v;
-    
     for(int i = 0; i < len; ++i) {
         hashVal = (hashVal + (s[i] - 'a' + 1)* Power[i])%mod;
         v.push_back(hashVal);
     }
     return v;
 }
-
 bool MATCH(pll a, pll b) {
     while(a.fi <= a.se) {
         if(s1[a.fi] != s2[b.fi])
@@ -43,27 +32,20 @@ bool MATCH(pll a, pll b) {
     }
     return 1;
 }
-
 void PowerGen(int n) {
     Power.resize(n+1);
     Power[0] = 1;
-    
     for(int i = 1; i < n; ++i)
         Power[i] = (Power[i-1] * p)%mod;
 }
-
-
 ll SubHash(vl &Hash, ll l, ll r, ll LIM) {
     ll H;
     H = (Hash[r] - (l-1 >= 0 ? Hash[l-1]:0) + mod)%mod;
     H = (H * Power[LIM-l])%mod;
-    
     return H;
 }
 
 // --------------- DOUBLE HASH GENERATORS ---------------
-
-
 // Generates Hash of entire string without PowerGen func
 vector<pair<ll, ll> > doubleHash(char *s, int len, ll mod1 = 1e9+7, ll mod2 = 1e9+9) {
     ll hashVal1 = 0, hashVal2 = 0, pPow1 = 1, pPow2 = 1;
@@ -77,7 +59,6 @@ vector<pair<ll, ll> > doubleHash(char *s, int len, ll mod1 = 1e9+7, ll mod2 = 1e
     }
     return v;
 }
-
 void PowerGen(int n) {
     Power.resize(n+1);
     Power[0] = {1, 1};
@@ -85,7 +66,6 @@ void PowerGen(int n) {
         Power[i].first = (Power[i-1].first * p)%mod1;
         Power[i].second = (Power[i-1].second * p)%mod2;
 }}
-
 vll doubleHash(char *s, int len) {      // Returns Double Hash vector for a full string
     ll hashVal1 = 0, hashVal2 = 0;
     vector<pll>v;
@@ -96,24 +76,20 @@ vll doubleHash(char *s, int len) {      // Returns Double Hash vector for a full
     }
     return v;
 }
-
 pll SubHash(vll &Hash, ll l, ll r, ll LIM) {    // Produce SubString Hash
     pll H;
     H.fi = (Hash[r].fi - (l-1 >= 0 ? Hash[l-1].fi:0) + mod1)%mod1;
     H.se = (Hash[r].se - (l-1 >= 0 ? Hash[l-1].se:0) + mod2)%mod2;
-    
     H.fi = (H.fi * Power[LIM-l].fi)%mod1;
     H.se = (H.se * Power[LIM-l].se)%mod2;
     return H;
 }
-
 // Returns True if the Hashval of length len exists in subrange [l, r] of Hash vector
 bool MatchSubStr(int l, int r, vector<pll>&Hash, pll HashVal, int len) {
     for(int Start = l, End = l+len-1; End <= r; ++End, ++Start) {
-        pll pattHash;
+        pll pattHash, strHash;
         pattHash.first = (HashVal.first*Power[Start].first)%mod1;
         pattHash.second = (HashVal.second*Power[Start].second)%mod2;
-        pll strHash;
         strHash.first = (Hash[End].first - (Start == 0 ? 0:Hash[Start-1].first) + mod1)%mod1;
         strHash.second = (Hash[End].second - (Start == 0 ? 0:Hash[Start-1].second) + mod2)%mod2;
         if(strHash == pattHash) return 1;
