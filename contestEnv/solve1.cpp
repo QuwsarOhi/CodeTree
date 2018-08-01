d76913137705397b1c8687e2502004e345aae5ae
quwsarohi <quwsarohi@gmail.com>
2018-08-02 01:30:03 +0600

    lst

--- contestEnv/solve1.cpp
+++ contestEnv/solve1.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX                 1000100
+#define MAX                 1000009
 #define EPS                 1e-9
 #define INF                 1e7
 #define MOD                 1000000007
@@ -46,81 +46,115 @@ typedef vector<pair<ll, ll> >vll;
 //int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 //----------------------------------------------------------------------------------------------------------
 
-/*
+ll powerMOD(ll x, ll y, ll mod) {                   // Can find modular inverse by a^(MOD-2),  a and MOD must be co-prime
+    ll res = 1;
+    x %= mod;
+    while(y > 0) {
+        if(y&1) res = (res*x)%mod;          // If y is odd, multiply x with result
+        y = y >> 1, x = (x * x)%mod;
+    }
+    return res%mod;
+}
+
+bool miillerTest(int d, int n) {        
+    int a = 2 + rand() % (n - 4);       // Pick a random number in [2..n-2]. 
+    int x = powerMOD(a, d, n);               // Compute a^d % n
+    if (x == 1  || x == n-1)
+       return 1;
+    while (d != n-1) {                      // Keep squaring x while one of the following doesn't happen
+        x = (x * x) % n;                    // (i)   d does not reach n-1
+        d *= 2;                             // (ii)  (x^2) % n is not 1
+        if (x == 1)      return 0;          // (iii) (x^2) % n is not n-1
+        if (x == n-1)    return 1;
+    }
+    return 0;               // Return composite
+}
+
+bool isPrime(int n, int k = 10) {               // Higher value of k gives more accuracy (Use k >= 9)
+    if(n <= 1 || n == 4)  return 0;             // Corner cases
+    if(n <= 3) return 1;
+    int d = n - 1;                              // Find r such that n = 2^d * r + 1 for some r >= 1
+    while(d % 2 == 0)  d /= 2;
+    for(int i = 0; i < k; i++)                  // Iterate given nber of 'k' times
+        if(miillerTest(d, n) == 0)
+            return 0;
+    return 1;
+}
+
 int pd[MAX];                        // Contains minimum prime factor/divisor, for primes pd[x] = x
 vector<int>primes;                  // Contains prime numbers
 void SieveLinear(int N) {
+    pd[1] = 1;
     for(int i = 2; i <= N; ++i) {
         if(pd[i] == 0) pd[i] = i, primes.push_back(i);                  // if pd[i] == 0, then i is prime
         for(int j=0; j < (int)primes.size() && primes[j] <= pd[i] && i*primes[j] <= N; ++j)
             pd[i*primes[j]] = primes[j];
-}}*/
-
-int pd[MAX];                        // Contains minimum prime factor/divisor, for primes pd[x] = x
-vector<int>primes;                  // Contains prime numbers
-vector<int>PF[MAX];
-void SieveLinearRangePF(int N, ll low, ll hi) {             // also returns unique prime factors in range [low, hi]
-    for(int i = 2; i <= N; ++i) {
-        if(pd[i] == 0) {
-            pd[i] = i, primes.push_back(i);                  // if pd[i] == 0, then i is prime
-            for(ll x = (low-1)-(low-1)%i+i; x <= hi; x += i)        // inserting all prime factors [prime will be inserted only once]
-                PF[x-low].push_back(i);                             // just to be sure, used low-1, instead of low
-        }
-        for(int j=0; j < (int)primes.size() && primes[j] <= pd[i] && i*primes[j] <= N; ++j)
-            pd[i*primes[j]] = primes[j];
 }}
 
-ll low, hi;
+unordered_map<int, int>MP;
 
-ll RET(ll n) {
-    ll T = n, mul = 1, tmp;
-    priority_queue<ll>pq;
+bool checkOK2(int &x, int k) {
+    // if this is prime return 0
     
-    //for(auto it : PF[n-low]) cout << it << " ";
-    //cout << endl;
+    if(isPrime(x))
+        return 0;
     
-    for(auto it : PF[n-low]) {
-        ll cnt = 0;
-        //cout << it << " : ";
-        if(T == 1) break;
-        while(T%it == 0) {
-            ++cnt;
-            T/=it;
-        }
-        pq.push(cnt+1);
-        mul *= cnt+1;
-        //cout << cnt+1 << endl;
+    for(auto p : primes) {
+        if(p > x or p > k) break;
+        while(x%p == 0)
+            x /= p;
     }
     
-    if(T != 1) {
-        mul *= 2;
-        pq.push(2);
+    if(x <= k) {
+        x = 1;
+        return 1;
     }
-    
-    ll ret = 0;
-    while(not pq.empty() and pq.top() != 1) {
-        ret += mul;
-        tmp = pq.top();
-        pq.pop();
-        mul /= tmp;
-        tmp--;
-        mul *= tmp;
-        if(tmp>1)
-            pq.push(tmp);
-    }
-    return ret;
+    return 0;
 }
 
+bool checkOK1(int &x, int k) {
+    //cerr << "GOT " << x << endl;
+    while(x > k) {
+        int T = pd[x];
+        if(T > k) break;
+        //cerr << x << " / " << pd[x] << endl;
+        x /= T;
+    }
+    
+    if(x <= k) {
+        x = 1;
+        return 1;
+    }
+    return 0;
+}
+
+const int LIM = 31700;
 int main() {
-    cin >> low >> hi;
-    //low = 10, hi = 20;
-    SieveLinearRangePF(1000010, low, hi);
+    SieveLinear(LIM);
+    int T, n, k, x;
+    scanf("%d", &T);
     
-    ll ans = 0;
-    for(ll i = low; i <= hi; ++i)
-        ans += RET(i);
-    
-    cout << ans << endl;
-    //cout << RET(12) << endl;    
+    while(T--) {
+        bool ok = 0;
+        int gcd;
+        scanf("%d%d", &n, &k);
+        for(int i = 0; i < n; ++i) {
+            scanf("%d", &x);
+            
+            if(ok) continue;
+            
+            //while(x%2==0) x/=2;
+            
+            if(x < LIM)    
+                ok |= checkOK1(x, k);
+            else
+                ok |= checkOK2(x, k);
+            
+            if(i == 0) gcd = x;
+            else gcd = __gcd(gcd, x);
+        }
+        
+        printf("%s\n", (gcd==1?"YES":"NO"));
+    }
     return 0;
 }
