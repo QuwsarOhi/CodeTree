37420c769fabb2db09a2416dbe366316f26d88c5
quwsarohi <quwsarohi@gmail.com>
2018-08-13 21:10:42 +0600

    uva

--- temp/solve3.cpp
+++ temp/solve3.cpp
@@ -1,3 +1,7 @@
+// UVa
+// 11348 - Exhibition
+// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=24&problem=2323&mosmsg=Submission+received+with+ID+21797329
+
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX                 110
@@ -46,4 +50,63 @@ typedef vector<pair<ll, ll> >vll;
 //int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 //----------------------------------------------------------------------------------------------------------
 
+int cnt[10010];
+bitset<10010>found, tot;
+vi v[60];
 
+int main() {
+    //fileRead("in");
+    //fileWrite("out");
+    
+    int t, x, n, m;
+    sf("%d", &t);
+    
+    for(int Case = 1; Case <= t; ++Case) {
+        sf("%d", &n);
+        tot.reset();
+        memset(cnt, 0, sizeof cnt);
+        
+        for(int i = 0; i < n; ++i) {
+            sf("%d", &m);
+            v[i].clear();
+            found.reset();
+            v[i].pb(m);
+            for(int j = 0; j < m; ++j) {
+                sf("%d", &x);
+                if(not found[x])
+                    found[x] = 1, cnt[x]++;
+                v[i].pb(x);
+                tot[x] = 1;
+            }
+        }
+        
+        pf("Case %d:", Case);
+        int unique = 0;
+        for(int i = 0; i < n; ++i) {
+            found.reset();
+            for(int j = 1; j < v[i][0]+1; ++j) {
+                if(not found[v[i][j]]) {
+                    found[v[i][j]] = 1;
+                    unique += cnt[v[i][j]] == 1;
+                }
+            }
+        }
+        
+        //cerr << unique << endl;
+        for(int i = 0; i < n; ++i) {
+            double sum = 0;
+            found.reset();
+            for(int j = 1; j < v[i][0]+1; ++j) {
+                if(not found[v[i][j]]) {
+                    found[v[i][j]] = 1;
+                    sum += cnt[v[i][j]] == 1;
+                }
+            }
+            //cerr << i << " " << sum << " " << (sum/unique)*100 << endl;
+            if(unique == 0) pf(" 0.000000");
+            else            pf(" %.6f%%", (sum/unique)*100);
+        }
+        pf("\n");
+    }
+    return 0;
+}
