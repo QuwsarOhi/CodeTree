e8809b208142c31e4be5694dbd2a85355f5ae50d
unknown <quwsarohi@gmail.com>
2018-09-24 21:15:20 +0600

    hard

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX                 200010
+// LightOJ
+// 1100 - Again Array Queries
+// http://lightoj.com/volume_showproblem.php?problem=1100
+
 #define EPS                 1e-9
 #define INF                 1e7
 #define MOD                 1000000007
@@ -46,3 +50,77 @@ typedef vector<pair<ll, ll> >vll;
 //int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 //----------------------------------------------------------------------------------------------------------
 
+struct QQuery {
+    int l, r, id;
+};
+
+const int block = 450;          // For 100000   : 320
+QQuery q[10100];
+int ans[10100], v[100100];
+
+// extra DS
+int cnt[1010];
+
+bool cmp2(const QQuery &a, const QQuery &b){                       // Faster Comparison function
+    if(a.l/block !=b.l/block)   return a.l < b.l;
+    if((a.l/block)&1)           return a.r < b.r;
+    return a.r > b.r;
+}
+
+void init() {
+    memset(cnt, 0, sizeof cnt);
+}
+
+void add(int idx) {
+    ++cnt[v[idx]];
+}
+
+void remove(int idx) {
+    --cnt[v[idx]];
+}
+
+int process() {
+    int ret = INF;
+    for(int i = 1, pst = INF; i <= 1000; ++i) {
+        if(cnt[i] == 1) {
+            ret = min(ret, abs(i-pst));
+            pst = i;
+        }
+        else if(cnt[i] > 1)
+            return 0;
+    }
+    return ret;
+}
+
+int main() {
+    //fileRead("in");
+    //fileWrite("out");
+
+    int t, n, Q;
+    sf("%d", &t);
+
+    for(int Case = 1; Case <= t; ++Case) {
+        sf("%d%d", &n, &Q);
+        for(int i = 0; i < n; ++i) sf("%d", &v[i]);
+
+        for(int i = 0; i < Q; ++i) {
+            sf("%d%d", &q[i].l, &q[i].r);
+            q[i].id = i;
+        }
+
+        sort(q, + q+Q, cmp2);
+        int l = 0, r = -1;
+        init();
+        for(int i = 0; i < Q; ++i) {
+            while(l > q[i].l)   add(--l);
+            while(r < q[i].r)   add(++r);
+            while(l < q[i].l)   remove(l++);
+            while(r > q[i].r)   remove(r--);
+            ans[q[i].id] = process();
+        }
+
+        pf("Case %d:\n", Case);
+        for(int i = 0; i < Q; ++i)
+            pf("%d\n", ans[i]);
+    }
+}
--- temp/solve2.cpp
+++ temp/solve2.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX                 300000
+#define MAX                 100100
 #define EPS                 1e-9
 #define INF                 1e7
 #define MOD                 1000000007
@@ -53,3 +53,110 @@ template<class T> using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_
 //int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
 //----------------------------------------------------------------------------------------------------------
 
+vector<int> G[MAX], Tree[MAX];
+vector<pair<int, int> >ans;
+int dfs_num[MAX], dfs_low[MAX], parent[MAX], dfsCounter;
+
+void bridge(int u) {
+    // dfs_num[u] is the dfs counter of u node
+    // dfs_low[u] is the minimum dfs counter of u node (it is minimum if a backedge exists)
+    dfs_num[u] = dfs_low[u] = ++dfsCounter;
+    for(int i = 0; i < (int)G[u].size(); i++) {
+        int v = G[u][i];
+        if(dfs_num[v] == 0) {
+            parent[v] = u;
+            bridge(v);
+            // if dfs_num[u] is lower than dfs_low[v], then there is no back edge on u node
+            // so u - v can be a bridge
+            if(dfs_num[u] < dfs_low[v])
+                ans.push_back(make_pair(min(u, v), max(u, v)));
+            // obtainig lower dfs counter (if found) from child nodes
+            dfs_low[u] = min(dfs_low[u], dfs_low[v]);
+        }
+        // if v is not parent of u then it is a back edge
+        // also dfs_num[v] must be less than dfs_low[u]
+        // so we update it
+        else if(parent[u] != v)
+            dfs_low[u] = min(dfs_low[u], dfs_num[v]);
+}}
+
+int BuildNewTree(int V) {
+    memset(dfs_num, 0, sizeof dfs_num);
+    int ncnt = 1;                                   // Tree's node starts from 1
+    for(int i = 1; i <= V; ++i)                     // dfs_low[i] contains new tree node mapping
+        if(dfs_num[dfs_low[i]] == 0)                // from the previous graph
+            dfs_num[dfs_low[i]] = ncnt++;
+
+    for(int i = 1; i <= V; ++i)
+        dfs_low[i] = dfs_num[dfs_low[i]];
+
+    for(auto it : ans) {
+        int u = it.first, v = it.second;
+        Tree[dfs_low[u]].pb(dfs_low[v]);
+        Tree[dfs_low[v]].pb(dfs_low[u]);
+        cerr << dfs_low[u] << " -- " << dfs_low[v] << endl;
+        cerr << "Actual " << u << " -- " << v << endl;
+    }
+    return ncnt;
+}
+
+int FindBridge(int V){                             //Bridge finding code
+    memset(dfs_num, 0, sizeof(dfs_num));
+    dfsCounter = 0;
+    for(int i = 1; i <= V; i++)
+        if(dfs_num[i] == 0) 
+            bridge(i);
+    for(int i = 1; i <= V; ++i)
+        cerr << i << " :: " << dfs_num[i] << " :: " << dfs_low[i] << endl;
+    return BuildNewTree(V);
+}
+
+int level[MAX], sparse[MAX][20];
+
+void dfs(int u, int par, int lvl) {                 // Tracks distance as well (From root 1 to all nodes)
+    level[u] = lvl;                                 // parent[] and level[] is necessary
+    parent[u] = par;                                
+    for(int i = 0; i < (int)Tree[u].size(); ++i)
+        if(parent[u] != G[u][i])
+            dfs(Tree[u][i], u, lvl+1);
+}
+
+void LCAinit(int V) {
+    memset(parent, -1, sizeof parent);
+    dfs(1, -1, 0);                               // DFS first
+    memset(sparse, -1, sizeof sparse);              // Main initialization of sparse table LCA starts here
+    for(int u = 1; u <= V; ++u)                     // node u's 2^0 parent
+        sparse[u][0] = parent[u];
+    for(int p = 1, v; (1LL<<p) <= V; ++p)
+        for(int u = 1; u <= V; ++u)
+            if((v = sparse[u][p-1]) != -1)      // node u's 2^x parent = parent of node v's 2^(x-1) [ where node v : (node u's 2^(x-1) parent) ]
+                sparse[u][p] = sparse[v][p-1];
+}
+
+int LCA(int u, int v) {
+    if(level[u] > level[v]) swap(u, v);         // v is deeper
+    int p = ceil(log2(level[v]));
+    for(int i = p ; i >= 0; --i)                // Pull up v to same level as u
+        if(level[v] - (1LL<<i) >= level[u])
+            v = sparse[v][i];
+    if(u == v) return u;                // if u WAS the parent
+    for(int i = p; i >= 0; --i)                                     // Pull up u and v together while LCA not found
+        if(sparse[v][i] != -1 && sparse[u][i] != sparse[v][i])      // -1 check is if 2^i is out of calculated range
+            u = sparse[u][i], v = sparse[v][i];
+    return parent[u];
+}
+
+int main() {
+    int V, E, Q, u, v;
+    sf("%d%d%d", &V, &E, &Q);
+
+    for(int i = 0; i < E; ++i){
+        sf("%d%d", &u, &v);
+        G[u].pb(v);
+        G[v].pb(u);
+    }
+
+    int treeV = FindBridge(V);
+    LCAinit(treeV);
+    return 0;
+}
