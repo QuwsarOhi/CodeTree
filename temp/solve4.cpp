d474fe437c40f954c353c8b34b4566649cdb1372
unknown <quwsarohi@gmail.com>
2018-09-28 13:13:09 +0600

    int

--- temp/solve4.cpp
+++ temp/solve4.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX                 10010
+#define MAX                 200100
 #define EPS                 1e-9
 #define INF                 1e7
 #define MOD                 1000000007
@@ -40,3 +40,107 @@ typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<pair<int, int> > vii;
 typedef vector<pair<ll, ll> >vll;
+
+
+// Suffix Array
+// Complexity : N log(N)
+
+int o[2][MAX], t[2][MAX];
+int idxToRank[MAX], rankToIdx[MAX], A[MAX], B[MAX], C[MAX], D[MAX];
+
+void SuffixArray(char str[], int len, int maxAscii = 256) {
+    int x = 0;
+    memset(A, 0, sizeof A);
+    memset(C, 0, sizeof C);
+    memset(D, 0, sizeof D);
+    memset(o, 0, sizeof o);
+    memset(t, 0, sizeof t);
+
+    for(int i = 0; i < len; ++i) A[(str[i]-'a')] = 1;
+    for(int i = 1; i < maxAscii; ++i) A[i] += A[i-1];
+    for(int i = 0; i < len; ++i) o[0][i] = A[(int)(str[i]-'a')];
+ 
+    for (int j = 0, jj = 1, k = 0; jj < len && k < len; ++j, jj <<= 1) {
+        memset(A, 0, sizeof A);
+        memset(B, 0, sizeof B);
+        for(int i = 0; i < len; ++i) {
+            ++A[ t[0][i] = o[x][i] ];
+            ++B[ t[1][i] = (i+jj<len) ? o[x][i+jj] : 0 ];
+        }
+        for(int i = 1; i <= len; ++i) {
+            A[i] += A[i-1];
+            B[i] += B[i-1];
+        }
+        for(int i = len-1; i >= 0; --i)
+            C[--B[t[1][i]]] = i;
+        for(int i = len-1; i >= 0; --i)
+            D[--A[t[0][C[i]]]] = C[i];
+        x^=1;
+        o[x][D[0]] = k = 1;
+        for(int i = 1; i < len; ++i)
+            o[x][D[i]]= (k += (t[0][D[i]] != t[0][D[i-1]] || t[1][D[i]] != t[1][D[i-1]]));
+    }
+    for(int i = 0; i < len; i++) {
+        idxToRank[i] = o[x][i]-1;
+        rankToIdx[o[x][i]-1] = i;
+}}
+
+// Longest Common Prefix: Kasai's Algorithm
+// Complexity: O(n)
+
+int lcp[MAX];
+void Kasai(int len, char str[]) {               // Matches Same charechters with i'th rank & (i+1)'th rank
+    int match = 0;
+    for(int idx = 0; idx < len; ++idx) {
+        if(idxToRank[idx] == len-1) {
+            match = 0;
+            continue;
+        }
+        int nxtRankIdx = rankToIdx[idxToRank[idx]+1];
+        while(idx+match < len and nxtRankIdx+match < len and str[idx+match] == str[nxtRankIdx+match])
+            ++match;
+        lcp[nxtRankIdx] = match;                // the lcp match of i'th & (i+1)'th is stored in
+        match -= match>0;                       // the index of (i+1)'th suffix's index
+}}
+
+
+// Longest Common Prefix [Sparse Table after running Kasai]
+
+int table[MAX][14];
+void buildSparseTableRMQ(int n) {                           //  O(n Log n)
+    for(int i = 0; i < n; ++i)
+        table[i][0] = i;
+    for(int j = 1; (1 << j) <= n; ++j)                      // 2^j
+        for(int i = 0; i + (1 << j) - 1< n; ++i) {          // For every node
+            if(lcp[suff[table[i][j-1]].idx] < lcp[suff[table[i + (1 << (j-1))][j-1]].idx])
+                table[i][j] = table[i][j-1];
+            else
+                table[i][j] = table[i + (1 << (j-1))][j-1];
+}}
+
+int sparseQueryRMQ(int l, int r) {                          // Gives LCP of index l, r in O(1)
+    int k = log2(r - l + 1);                                // log(2);
+    return min(lcp[suff[table[l][k]].idx], lcp[suff[table[r - (1 << k) + 1][k]].idx]);
+}
+
+
+char str[3*MAX], tok[MAX];
+
+int main() {
+	int n, q, len = 0;
+	sf("%d%d", &n, &q);
+
+	for(int i = 0; i < n; ++i) {
+		sf("%s", tok);
+		int tokLen = strlen(tok);
+		
+		if(len != 0)
+			str[len++] = '#';
+
+		for(int j = 0; j < tokLen; ++j)
+			str[len++] = tok[j];
+	}
+
+	cout << str << endl;
+	return 0;
+}
