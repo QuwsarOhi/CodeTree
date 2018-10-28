be1c4961400ce3057f4085534de4755ae97490ca
quwsarohi <quwsarohi@gmail.com>
2018-10-28 13:03:21 +0630

    auto

--- temp/solve2.cpp
+++ temp/solve2.cpp
@@ -1,108 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX                 510000
+#define EPS                 1e-9
+#define INF                 0x3f3f3f3f
+#define MOD                 1000000007
+#define pb                  push_back
+#define mp                  make_pair
+#define xx                  first
+#define yy                  second
+#define pi                  acos(-1)
+#define pf                  printf
+#define sf(XX)              scanf("%lld", &XX)
+#define SIZE(a)             ((ll)a.size())
+#define ALL(S)              S.begin(), S.end()              
+#define Equal(a, b)         (abs(a-b) < EPS)
+#define Greater(a, b)       (a >= (b+EPS))
+#define GreaterEqual(a, b)  (a > (b-EPS))
+#define FOR(i, a, b)        for(register int i = (a); i < (int)(b); ++i)
+#define FORR(i, a, b)       for(register int i = (a); i > (int)(b); --i)
+#define FastIO              ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FileRead(S)         freopen(S, "r", stdin);
+#define FileWrite(S)        freopen(S, "w", stdout);
+#define Unique(X)           X.erase(unique(X.begin(), X.end()), X.end())
+#define STOLL(X)            stoll(X, 0, 0)
 
-int d[8][2] = { {0, 1}, {-1, 1}, {1, 1}, 
-				{0, -1}, {-1, -1}, {1, -1},
-				{-1, 0},
-				{1, 0}
-			};
+#define isOn(S, j)          (S & (1 << j))
+#define setBit(S, j)        (S |= (1 << j))
+#define clearBit(S, j)      (S &= ~(1 << j))
+#define toggleBit(S, j)     (S ^= (1 << j))
+#define lowBit(S)           (S & (-S))
+#define setAll(S, n)        (S = (1 << n) - 1)
 
-int nn, mm;
-int cnt[60][60];
-pair<int, int>p[60];
-map<int, int, greater<int> >MAP;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef map<int, int> mii;
+typedef map<ll, ll>mll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<ll>vl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pair<int, int> > vii;
+typedef vector<pair<ll, ll> >vll;
 
-void dfs(int x, int y, int id) {
-	if(x < 0 or y < 0 or x >= nn or y >= mm) return;
-	++cnt[x][y];
-	dfs(x+d[id][0], y+d[id][1], id);
-}
+//int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
+//int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+//----------------------------------------------------------------------------------------------------------
 
-void PP(int y) {
-	int x = MAP[y];
-	if(x == -1 or y == 0)
-		printf("*");
-	else
-		printf("%d", x);
-}
+int v[1000];
 
 int main() {
-	freopen("out", "w", stdout);
-	
-	int n, c, pos;
-	char s;
+	int t, n;
+	cin >> t;
 
-	scanf("%d", &n);
-	
-	for(int i = 0; i < n; ++i) {
-		scanf("%d", &p[i].first);
-		p[i].second = i+1;
+	for(int Case = 1; Case <= t; ++Case) {
+		cin >> n;
+		int XOR = 0;
+		int sum = 0;
+		for(int i= 0; i < n; ++i) {
+			cin >> v[i];
+			if(i) XOR ^= v[i];
+			else  XOR = v[i];
+			sum += v[i];
+		}
+
+		if(sum == n) {
+			if()
+		}
+
+		if(n == 1) {
+			cout << "Case " << Case << ": Enemy\n";
+			continue;
+		}
+		
+		if(XOR > 0)
+			cout << "Case " << Case << ": Me\n";
+		else
+			cout << "Case " << Case << ": Enemy\n";
 	}
-
-	scanf("%d%d%d", &nn, &mm, &c);
-
-	//cerr << "DONE\n";
-	for(int i = 0; i < c; ++i) {
-		scanf(" %c %d", &s, &pos);
-		//cerr << s << " " << pos << endl;
-		pos--;
-		if(s == 'L') {
-			dfs(pos, 0, 0);
-			dfs(pos-1, 0, 1);
-			dfs(pos+1, 0, 2);
-		}
-		else if(s == 'R') {
-			dfs(pos, mm-1, 3);
-			dfs(pos-1, mm-1, 4);
-			dfs(pos+1, mm-1, 5);
-		}
-		else if(s == 'D') {
-			dfs(nn-1, pos, 6);
-			dfs(nn-1, pos-1, 4);
-			dfs(nn-1, pos+1, 1);
-		}
-		else {
-			dfs(0, pos, 7);
-			dfs(0, pos-1, 5);
-			dfs(0, pos+1, 2);
-		}
-	}
-
-	//for(int i = 0; i < nn; ++i, printf("\n"))
-	//	for(int j = 0; j < mm; ++j)
-	//		printf("%2d ", cnt[i][j]);
-
-	sort(p, p+n, greater<pair<int, int> >());
-	for(int i = 0; i < nn; ++i)
-		for(int j = 0; j < mm; ++j)
-			MAP[cnt[i][j]] = -1;
-
-	if(MAP.size() < n) {
-		printf("No Solution Mr. Problem. Sorry!\n");
-		return 0;
-	}
-
-	int idx = 0;
-	for(auto it = MAP.begin(); it != MAP.end() and idx < n; ++it, ++idx)
-		it->second = p[idx].second;
-
-	for(int i = 0; i < nn; ++i, printf("\n")) {
-		PP(cnt[i][0]);
-		for(int j = 1; j < mm; ++j) {
-			printf(" ");
-			PP(cnt[i][j]);
-		}
-	}
-
 	return 0;
 }
-
-/*
-2
-1 2
-5 8 4
-U 3
-L 3
-R 4
-D 6
-*/
