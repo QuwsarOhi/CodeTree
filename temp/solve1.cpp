09595d21ec2e10fb7059d2c5c88a50ce963750ac
QuwsarOhi <quwsarohi@gmail.com>
2018-11-18 00:17:53 +0600

    DP AC

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,64 +1,48 @@
+// LightOJ
+// 1205 - Palindromic Numbers
+// Digit DP
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int tt[20], dp[18][18][2][2][2];
+int dp[18][18][2][2][2];
 vector<int>mx;
 
-int recur(int l, int r, bool nonZero, bool lftTight, bool rhtTight) {
+int recur(int l, int r, bool nonZero, bool lftTight, bool biggerRight) {
     if(l > r) {
-        // fuck this shit
-        for(int i = 0; i < mx.size(); ++i)
-            printf("%d", tt[i]);
-        //printf("\n");
-
-        if(lftTight == 1 && rightGreater == 1) {
-            printf(" NO\n");
+        if(lftTight == 1 && biggerRight == 1)
             return 0;
-        }
-
-        printf(" YES %d %d\n", lftTight, rightGreater);
         return 1;
     }
 
-    //int &ret = dp[l][r][nonZero][lftTight][rightGreater];
-    //if(ret != -1) {
-    //    cerr << "HIT mother fucker" << endl;
-    //    return ret;
-    //}
+    int &ret = dp[l][r][nonZero][lftTight][biggerRight];
+    if(ret != -1)
+        return ret;
+    ret = 0;
 
-    int ret = 0;
-    int lftlim = lftTight ? mx[l]:9;
-    int rhtlim = rhtTight ? mx[r]:9;
+    int lim = lftTight ? mx[l]:9;
 
-    //cerr << "Range :: " << l << " " << r << " :: " << lim << endl;
-    for(int d = 0; d <= lftlim; ++d) {
-        bool newTight = d == lftlim ? lftTight:0;
-        bool rhtTight = d == rhtlim ? rhtTight:0;
-        
-        if(l == r)
-            newRightGreater = rightGreater;
+    for(int d = 0; d <= lim; ++d) {
+        bool newNonZero = nonZero or d > 0;
+        bool newLftTight = d == mx[l] ? lftTight:0;
 
-        if(nonZero == 0 && d == 0) {
-            tt[l] = 0;
-            //cerr << "Place " << "0" << " range " << l << " " << r << endl;
-            ret += recur(l+1, r, nonZero, newTight, newRightGreater);
+        if(d == 0 && newNonZero == 0) {
+            ret += recur(l+1, r, newNonZero, newLftTight, biggerRight);
             continue;
         }
-
-        //cerr << "Place " << d << " range " << l << " " << r <<endl;
-        tt[l] = tt[r] = d;
-        ret += recur(l+1, r-1, 1, newTight, newRightGreater);
+        else if(l == r) {
+            ret += recur(l+1, r-1, newNonZero, newLftTight, biggerRight);
+            continue;
+        }
+        ret += recur(l+1, r-1, newNonZero, newLftTight, (d >= mx[r] and biggerRight) or d > mx[r]);
     }
 
     return ret;
 }
 
-
 int solve(long long x) {
     if(x < 0) return 0;
     if(x == 0) return 1;
-
-    cerr << "VAL " << x << endl;
 
     mx.clear();
     while(x) {
@@ -80,7 +64,7 @@ int main() {
         scanf("%lld%lld", &a, &b);
         if(a > b) swap(a, b);
 
-        printf("%d %d\n", solve(a-1), solve(b));
+        printf("Case %d: %d\n", Case, solve(b)-solve(a-1));
     }
 
     return 0;
