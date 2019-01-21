e4aa28f2c7e1b84be4a45905ad99ac0c131545aa
QuwsarOhi <quwsarohi@gmail.com>
2019-01-22 01:15:44 +0600

    ll

--- temp/solve7.cpp
+++ temp/solve7.cpp
@@ -1,65 +1,81 @@
+// UVa
+// 11536 - Smallest Sub-Array
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-vector<pair<int, int> > u, v;
+ll cnt[1005], x[1000009];
+set<ll>s;
+int k; 
+
+void genSeq(int n, int m) {
+	x[1] = 1, x[2] = 2, x[3] = 3;
+	
+	for(int i = 4; i <= n; ++i)
+		x[i] = (x[i-1] + x[i-2] + x[i-3])%m + 1;
+}
+
+bool canRemove(int v) {
+	if(v > k)
+		return 1;
+	if(cnt[v]-1 >= 1)
+		return 1;
+	return 0;
+}
+
+void Add(int v) {
+	if(v > k)
+		return;
+	cnt[v]++;
+	if(cnt[v] == 1)
+		s.insert(v);
+}
+
+void Remove(int v) {
+	if(v > k)
+		return;
+	cnt[v]--;
+	if(cnt[v] == 0)
+		s.erase(v);
+}
+
+int getSize(int n) {
+	int l = 1, r = 1, ans = 1000010;
+
+	s.clear();
+	memset(cnt, 0, sizeof cnt);
+	Add(x[1]);
+
+	while(r < n) {
+		while(l+1 <= r and canRemove(x[l]))
+			Remove(x[l++]);
+
+		if(s.size() == k)
+			ans = min(ans, r-l+1);
+
+		Add(x[++r]);
+		if(s.size() == k)
+			ans = min(ans, r-l+1);
+	}
+
+	return ans;
+}
 
 int main() {
-	int t, l, r, m;
+	int t, n, m;
 	scanf("%d", &t);
-	bool ff = 1;
 
-	while(t--) {
-		scanf("%d", &m);
+	for(int Case = 1; Case <= t; ++Case) {
+		scanf("%d%d%d", &n, &m, &k);
+		genSeq(n, m);
 
-		while(1) {
-			scanf("%d%d", &l, &r);
-			if(l == 0 and r == 0)
-				break;
-			v.push_back({l, r});
-		}
+		int ans = getSize(n);
 
-		sort(v.begin(), v.end());
-
-		u.clear();
-		for(int i = 0; i < v.size(); ++i) {
-			if(u.empty()) {
-				if(v[i].first <= 0)
-					u.push_back(v[i]);
-				else
-					break;
-			}
-
-			if(v[i].first <= 0) {
-				if(u.back().second < v[i].second) {
-					u.pop_back();
-					u.push_back(v[i]);
-				}
-			}
-
-			if(u[i-2].second >= u[i].first and u[i].second >= u[i-1].second) {
-				pair<int, int> t = u[i];
-				u.pop_back(), u.pop_back();
-				u.push_back(t);
-			}
-		}
-
-		for(auto it : u)
-			cout << it.first << ", " << it.second << endl;
-		cout << endl;
-
-		if(not ff)
-			printf("\n");
-		ff = 0;
-		if(u.empty() or u.back().second < m)
-			printf("0\n");
-		else {
-			printf("%d\n", (int)u.size());
-			for(int i = 0; i < u.size(); ++i)
-				printf("%d %d\n", u[i].first, u[i].second);
-		}
-
-		u.clear();
-		v.clear();
+		if(ans != 1000010)
+			printf("Case %d: %d\n", Case, ans);
+		else
+			printf("Case %d: sequence nai\n", Case);
 	}
 
 	return 0;
