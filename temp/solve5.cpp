d1d90864c8aa6abe30df8d1329461b865dd556a6
QuwsarOhi <quwsarohi@gmail.com>
2019-01-24 01:48:02 +0600

    lst

--- temp/solve5.cpp
+++ temp/solve5.cpp
@@ -1,47 +1,111 @@
 #include <bits/stdc++.h>
+#define MAX 100100
 using namespace std;
-#define MAX                 510000
-#define EPS                 1e-9
-#define INF                 0x3f3f3f3f
-#define MOD                 1000000007
-#define pb                  push_back
-#define mp                  make_pair
-#define xx                  first
-#define yy                  second
-#define pi                  acos(-1)
-#define pf                  printf
-#define sf(XX)              scanf("%lld", &XX)
-#define SIZE(a)             ((ll)a.size())
-#define ALL(S)              S.begin(), S.end()              
-#define Equal(a, b)         (abs(a-b) < EPS)
-#define Greater(a, b)       (a >= (b+EPS))
-#define GreaterEqual(a, b)  (a > (b-EPS))
-#define FastIO              ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define FileRead(S)         freopen(S, "r", stdin);
-#define FileWrite(S)        freopen(S, "w", stdout);
-#define Unique(X)           X.erase(unique(X.begin(), X.end()), X.end())
-#define STOLL(X)            stoll(X, 0, 0)
 
-#define isOn(S, j)          (S & (1 << j))
-#define setBit(S, j)        (S |= (1 << j))
-#define clearBit(S, j)      (S &= ~(1 << j))
-#define toggleBit(S, j)     (S ^= (1 << j))
-#define lowBit(S)           (S & (-S))
-#define setAll(S, n)        (S = (1 << n) - 1)
+int sparse[MAX][22], lvl[MAX], tot[MAX];
+vector<int>G[MAX];
 
-typedef unsigned long long ull;
-typedef long long ll;
-typedef map<int, int> mii;
-typedef map<ll, ll>mll;
-typedef map<string, int> msi;
-typedef vector<int> vi;
-typedef vector<ll>vl;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pair<int, int> > vii;
-typedef vector<pair<ll, ll> >vll;
+int dfs(int u, int par, int l) {
+	lvl[u] = l;
+	sparse[u][0] = par;
+	tot[u] = 1;
+	for(auto v : G[u])
+		if(v != par)
+			tot[u] += dfs(v, u, l+1);
+	return tot[u];
+}
 
-//int dx[] = {-1, 0, 1, 0}, dy[] = {0, 1, 0, -1};
-//int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-//----------------------------------------------------------------------------------------------------------
+int FindPar(int u, int p) {
+	for(int i = 20; i >= 0 and u >= 0; --i) {
+		if(p & (1 << i))
+			u = sparse[u][i];
+	}
+	return u;
+}
 
+int LCA(int a, int b) {
+	if(lvl[a] < lvl[b])
+		swap(a, b);
+
+	for(int i = 20; i >= 0; --i)
+		if(lvl[a] - (1 << i) >= lvl[b])
+			a = sparse[a][i];
+
+	if(a == b)
+		return a;
+
+	for(int i = 20; i >= 0; --i)
+		if(sparse[a][i] != sparse[b][i] and sparse[a][i] != -1)
+			a = sparse[a][i], b = sparse[b][i];
+
+	return sparse[a][0];
+}
+
+int Nodes(int a, int b) {
+	int d = abs(lvl[a] - lvl[b]) + 1;
+	return d;
+}
+
+
+pair<int, int> SubLCA(int a, int b) {
+	if(lvl[a] != lvl[b]) return make_pair(-MAX, -MAX);
+
+	int c = 0;
+	for(int i = 20; i >= 0; --i)
+		if(sparse[a][i] != sparse[b][i] and sparse[a][i] != -1)
+			a = sparse[a][i], b = sparse[b][i];
+
+	return make_pair(a, b);
+}
+
+int solve(int a, int b) {
+	if(a == b) return tot[1];
+
+	if(lvl[a] < lvl[b])			// b is on top
+		swap(a, b);
+
+	int c = LCA(a, b);
+	int d = lvl[a] + lvl[b] - 2*lvl[c] - 1;
+
+	if(d <= 0 or d%2 == 0)
+		return 0;
+
+	// if LCA is the answer
+	if(lvl[a] - lvl[c] == lvl[b] - lvl[c]) {
+		pair<int, int> x = SubLCA(a, b);
+		return tot[1] - tot[x.first] - tot[x.second];
+	}
+
+	// if answer is on left chain (parent of a)
+	int p = (d+1)/2;
+	return tot[FindPar(a, p)] - tot[FindPar(a, p-1)];
+}
+
+int main() {
+	int n, u, v;
+	cin >> n;
+
+	for(int i = 1; i < n; ++i) {
+		cin >> u >> v;
+		G[u].push_back(v);
+		G[v].push_back(u);
+	}
+
+	memset(sparse, -1, sizeof sparse);
+	tot[1] = dfs(1, -1, 0);
+
+	for(int p = 1; (1 << p) <= n; ++p)
+		for(u = 1; u <= n; ++u)
+			if(sparse[u][p-1] != -1)
+				sparse[u][p] = sparse[sparse[u][p-1]][p-1];
+
+	int q;
+	cin >> q;
+
+	while(q--) {
+		cin >> u >> v;
+		cout << solve(u, v) << endl;
+	}
+
+	return 0;
+}
