c59d898feb7d39b78f991edfc09d1c708f01ab26
Quwsar Ohi <quwsarohi@gmail.com>
2019-03-03 17:49:34 +0600

    Update solve1.cpp

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,101 +1,72 @@
 #include <bits/stdc++.h>
-#define dbug(x)     cerr << #x << " = " << x << endl
-#define MAX 300010
+#define MAX 300100
 using namespace std;
 typedef long long ll;
 
-ll v[MAX], seg[MAX];
 int block;
+ll val[MAX];
+vector<ll>seg[MAX];
 
-void build(int n) {
+void built(int n) {
     block = sqrt(n + .0) + 1;
-    int l = 0, r = 0;
-    for( ; r < n; ++r) {
-        if(r%block == 0 and r != 0) {
-            sort(seg+l, seg+r);
-            cerr << l << " -- " << r << endl;
-            l = r;
+    
+    for(int i = 0; i < n; ++i)
+        seg[i/block].push_back(val[i]);
+    
+    int len = n/block + 1;
+    for(int i = 0; i < len; ++i)
+        sort(seg[i].begin(), seg[i].end());
+}
+
+void update(int idx, ll newVal) {
+    int blockIdx = idx/block;
+    ll pst = val[idx];
+    for(int i = 0; i < seg[blockIdx].size(); ++i)
+        if(seg[blockIdx][i] == pst) {
+            seg[blockIdx][i] = newVal;
+            break;
         }
-        seg[r] = v[r];
-    }
-    cerr << l << " -- " << r << endl;
-    sort(seg+l, seg+r);
+    
+    val[idx] = newVal;
+    sort(seg[blockIdx].begin(), seg[blockIdx].end());
 }
 
-void getRange(int idx, int n, int &l, int &r) {
-    int segIndx = idx/block;
-    l = segIndx*block;
-    r = l+block-1;
-}
-
-void update(int idx, int n, ll val) {
-    int l, r;
-    getRange(idx, n, l, r);
-    ++r;
-
-    v[idx] = seg[idx] = val;
-    cerr << "Update at " << idx << " val " << val << " " << l << " -- " << min(r, n) << endl;
-    sort(seg+l, seg+min(r, n));
-}
-
-ll query(int ql, int qr, ll val) {
+ll query(int l, int r, ll qval) {
     ll ret = 0;
-    cerr << "Query " << ql << " " << qr << " val " << val << endl;
-    while(ql <= qr) {
-        if(ql % block == 0 and ql + block - 1 <= qr) {
-            int t = lower_bound(seg+ql, seg+ql+block, val) - seg;
-            ret += t - ql;
-            cerr << t - ql << endl;
-            ql += block;
+    for(int i=l; i<=r; ) {
+        if(i % block == 0 && i + block - 1 <= r) {
+            int idx = i/block;
+            ret += ll(lower_bound(seg[idx].begin(), seg[idx].end(), qval) - seg[idx].begin());
+            i += block;
         }
-        else
-            ret += v[ql++] < val;
+        else {
+            ret += val[i] < qval;
+            ++i;
+        }
     }
     return ret;
 }
 
-void printer(int n) {
-    cerr << "Block " << block <<  endl;
-    for(int i = 0; i < n; ++i) {
-        if(i > 0 and i%block == 0)
-            cerr << endl;
-        cerr << seg[i] << "(" << i << ") ";
-    }
-    cerr << endl;
-}
 
 int main() {
-    ll n, m, u, l, r, vv, p;
-    cin >> n >> m >> u;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    for(int i = 0; i < n; ++i) 
-        cin >> v[i];
-    build(n);
+    ll n, u, k, l, r, v, p, q;
 
-    while(m--) {
-        cin >> l >> r >> vv >> p;
-        --l, --r, --p;
-        ll k = query(l, r, vv);
-        update(p, n, (k*u)/(r-l+1));
+    cin >> n >> q >> u;
+    for(int i = 0; i < n; ++i)
+        cin >> val[i];
+
+    built(n);
+    while(q--) {
+        cin >> l >> r >> v >> p;
+        k = query(--l, --r, v);
+        update(--p, (u*k)/double(r-l+1));
     }
 
-    //printer(n);
-
     for(int i = 0; i < n; ++i)
-        cout << v[i] << "\n";
+        cout << val[i] << "\n";
 
     return 0;
 }
-
-/*
-
-10 1 11
-1 6 8 3 7 5 2 9 10 4
-
-10 1 11
-1 1 1 1 1 1 1 1 1 1
-
-10 1 11
-10 10 10 10 10 10 10 10 10 10
-
-*/
