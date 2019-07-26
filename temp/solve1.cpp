4f78f4bacd4e81727ad28e7b9052a96e8a27eb48
quwsarohi <quwsarohi@gmail.com>
2019-07-27 02:55:51 +0600

    lst

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,48 +1,34 @@
 #include <bits/stdc++.h>
-#define MAX 100009
-#define MOD 1000000007
 using namespace std;
 typedef long long ll;
-typedef vector<long long> vl;
 
-int st[100009], ed[100009];
+ll n, k, s;
+map<ll, ll> st;
 
-int main() {
-    int t;
-    cin >> t;
+void recur(ll val, ll step) {
+	if(step >= k) {
+		st[val]+=1;
+		return;
+	}
 
-    string s;
-    while(t--) {
-        cin >> s;
-
-        for(int i = 0; i < s.size(); ++i) {
-            st[i] = (i > 0) ? st[i-1]:0;
-            st[i] += (s[i] == '(');
-        }
-
-        for(int i = (int)s.size()-1; i >= 0; --i) {
-            ed[i] = ed[i+1];
-            ed[i] += (s[i] == ')');
-        }
-
-        ll ans = 0;
-        for(int i = 0; i < s.size(); ++i) {
-            //cerr << st[i] << " " << ed[i] << endl;
-            ans = max(ans, (ll)min(st[i], ed[i]));
-        }
-
-        cout << ans*2 << endl;
-    }
-    
-    return 0;
+	for(int i = 0; i <= n; ++i)
+		recur(val^i, step+1);
 }
 
+int main() {
+	ll t;
+	cin >> t;
 
-/*
+	while(t--) {
+		cin >> n >> k >> s;
 
-3
-(()())
-(()(()))
-()()()()()
+		st.clear();
+		recur(s, 0);
 
-*/
+		for(auto it : st)
+			cout << it.first << " : " << it.second << endl;
+		cout << endl;
+	}
+
+	return 0;
+}
