9ed66cd222b6168a2c0ed407d7858e07b65bf97b
Quwsar Ohi <quwsarohi@gmail.com>
2020-06-10 16:46:56 +0600

    slack

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,12 +1,40 @@
-#include <bits/stdc++.h>
-typedef long long ll;
+#include <iostream>
+#include <string>
 using namespace std;
 
+class CSE42 {
+	public:
+		string name = "";
+		int id = 0;
+		int section = 0;
+		int intake = 42;
+
+	CSE42() {}
+
+	CSE42(string name, int id, int section) {
+		this->name = name;
+		this->id = id;
+		this->section = section;
+	}
+};
 
 
 int main() {
+	CSE42 array[5];
 
-	
+	for(int i = 0; i < 5; ++i) {
+		string name = "Student_";
+		name.push_back('1'+i);
+		array[i] = CSE42(name, i+1, i+1);
+	}
+
+	for(int i = 0; i < 5; ++i) {
+		cout << "Name: " << array[i].name << endl;
+		cout << "Intake: " << array[i].intake << endl;
+		cout << "Section: " << array[i].section << endl; 
+		cout << "ID: " << array[i].id << endl;
+		cout <<"\n";
+	}
 
 	return 0;
 }
--- temp/solve3.cpp
+++ temp/solve3.cpp
@@ -1,8 +1,28 @@
-#include <bits/stdc++.h>
-#define MAX 200009
-typedef long long ll;
+#include <iostream>
 using namespace std;
 
+class A{
+	int a, b, c;
+	
+public:
+	A(int x, int y, int z) {
+		a = x;
+		b = y;
+		c = z;
+	}
+};
+
+void printValue(A val) {
+	int *p = (int*) &val;
+	cout << "a = " << *p << endl;
+	p++;
+	cout << "b = " << *p << endl;
+	p++;
+	cout << "c = " << *p << endl;
+}
+
 int main() {
-    
+	A y = A(1, 2, 3);
+	printValue(y);
+	return 0;
 }
