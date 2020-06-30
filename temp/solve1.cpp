fe7166176761acdc5e32125500cbf2e22c73ad24
Quwsar Ohi <quwsarohi@gmail.com>
2020-07-01 00:55:20 +0600

    copy checker added

--- temp/solve1.cpp
+++ temp/solve1.cpp
@@ -1,40 +1,66 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
-
-class CSE42 {
-	public:
-		string name = "";
-		int id = 0;
-		int section = 0;
-		int intake = 42;
-
-	CSE42() {}
-
-	CSE42(string name, int id, int section) {
-		this->name = name;
-		this->id = id;
-		this->section = section;
-	}
-};
 
 
 int main() {
-	CSE42 array[5];
+	long long a, b, sum = 0;
+	// 1
+	cin >> a >> b;
 
-	for(int i = 0; i < 5; ++i) {
-		string name = "Student_";
-		name.push_back('1'+i);
-		array[i] = CSE42(name, i+1, i+1);
+	//2 3
+	if(a%2 == 0) {
+		cout << a << " is even\n";
+		sum += a;
+	}
+	else {
+		cout << a << " is odd\n";
+		sum -= a;
 	}
 
-	for(int i = 0; i < 5; ++i) {
-		cout << "Name: " << array[i].name << endl;
-		cout << "Intake: " << array[i].intake << endl;
-		cout << "Section: " << array[i].section << endl; 
-		cout << "ID: " << array[i].id << endl;
-		cout <<"\n";
+
+	// 4 5 6
+	if(b > 0) {
+		cout << b << " is greater than zero\n";
+		sum += b;
 	}
+	else if(b < 0) {
+		cout << b << " is less than zero\n";
+		sum -= b;
+	}
+	else {
+		cout << b << " is equal to zero\n";
+		sum *= b;
+	}
+
+	// 7
+	cout << "after adding,a+b is equal to " << a+b << "\n";
+
+
+	// 8 9
+	if((a+b)%2 == 0) {
+		sum += a;
+		sum += b;
+		cout << "after adding " << a << " " << b << " sum is equal to " << sum << "\n";
+	}
+	else {
+		sum -= a;
+		sum -= b;
+		cout << "after removing " << a << " " << b << " b sum is equal to " << sum << "\n";
+	}
+
+	// 10
+	if(a+b == 0) {
+		sum *= 10;
+		cout << "the result after multiplying sum = " << sum << "\n";
+	}
+
+	// 11
+	sum *= 5;
+
+	// 12
+	cout << "THE ULTIMATE RESULT = " << sum << "\n";
+	// 13
+	cout << "I wanna be a national contestant and I can write long code\n";
 
 	return 0;
 }
