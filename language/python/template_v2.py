04dfeb9aed90d1bd2a5552074017b5117a13c179
Quwsar Ohi <quwsarohi@gmail.com>
2023-03-13 18:33:53 -0600

    bruteforce added

--- language/python/template_v2.py
+++ language/python/template_v2.py
@@ -5,12 +5,30 @@ from sys import stdin, stdout
 #setrecursionlimit(1000000)
 #print(getrecursionlimit())
 
-
 def input(): 
     return stdin.readline()
+
 
 # use f-string
 def print(inp, end='\n'):
     stdout.write(inp+end)
+
+
+# for genearing all possible & all permutation solution
+def gen_comb(N: int, permute:bool=False):
+    from itertools import permutations as permutations
+    for i in range(1 << N):
+        p = []
+
+        for j in range(N):
+            if i & (1 << j) > 0:
+                p.append(j)
+        
+        if len(p) == 0: continue
+
+        for perm in permutations(p):
+            yield perm
+
+# ---------------------- Code Starts ----------------------
 
 print(f"This is a {12}")
